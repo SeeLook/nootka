c72f84ef6a3ac4781a7e934f313806eb902b00e7
SeeLook <945374+SeeLook@users.noreply.github.com>
2011-02-27 12:08:06 +0100

    first

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
-#include <QtCore/QCoreApplication>
+#include <QtGui/QApplication>
+#include "nutoukmainwindow.h"
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
+    QApplication a(argc, argv);
+    NutoukMainWindow w;
+    w.show();
 
     return a.exec();
 }
