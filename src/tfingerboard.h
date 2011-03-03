5290d97a4ed6c3c923b85c4502e4ad8674d00dd7
Tomasz Bojczuk <tomaszbojczuk@gmail.com>
2011-03-03 13:32:13 +0100

    03.03 final

---
+++ src/tfingerboard.h
@@ -0,0 +1,18 @@
+#ifndef TFINGERBOARD_H
+#define TFINGERBOARD_H
+
+#include <QWidget>
+
+class TfingerBoard : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit TfingerBoard(QWidget *parent = 0);
+
+signals:
+
+public slots:
+
+};
+
+#endif // TFINGERBOARD_H
