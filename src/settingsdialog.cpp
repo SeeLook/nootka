6dfbcc5bea17cf7db575a52f52e587adc832e912
Tomasz Bojczuk <tomaszbojczuk@gmail.com>
2011-03-20 18:42:30 +0100

    20.03

--- src/settingsdialog.cpp
+++ src/settingsdialog.cpp
@@ -1,3 +1,21 @@
+/***************************************************************************
+ *   Copyright (C) 2011 by Tomasz Bojczuk  				   *
+ *   tomaszbojczuk@gmail.com   						   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License	   *
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
+ ***************************************************************************/
+
 #include "settingsdialog.h"
 #include "tglobals.h"
 #include "tnoteview.h"
@@ -90,7 +108,7 @@ GuitarSettings::GuitarSettings(QWidget *parent) :
 
     QHBoxLayout *upLay = new QHBoxLayout;
     QGroupBox *tuneGr = new QGroupBox(tr("tune of the guitar"));
-    tuneGr->setStatusTip(tr("Select apropirate tune from list or prepare own tune."));
+    tuneGr->setStatusTip(tr("Select appropirate tune from list or prepare own tune."));
     QVBoxLayout *tuneLay = new QVBoxLayout;
     tuneLay->setAlignment(Qt::AlignCenter);
     tuneCombo = new QComboBox(this);
