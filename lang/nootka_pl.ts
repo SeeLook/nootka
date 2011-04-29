bb837d30f8467dd96e0513efca938350956b4678
SeeLook <945374+SeeLook@users.noreply.github.com>
2011-04-29 12:28:55 +0200

    29kwi2011,

--- lang/nootka_pl.ts
+++ lang/nootka_pl.ts
@@ -1,2 +1,69 @@
-
-
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="pl_PL">
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../src/exam/examsettingsdlg.cpp" line="29"/>
+        <source>exam&apos;s settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/exam/examsettingsdlg.cpp" line="30"/>
+        <source>Levels (*.nlv)</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>examSettingsDlg</name>
+    <message>
+        <location filename="../src/exam/examsettingsdlg.cpp" line="38"/>
+        <source>Levels</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/exam/examsettingsdlg.cpp" line="41"/>
+        <source>Questions</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/exam/examsettingsdlg.cpp" line="44"/>
+        <source>Range</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/exam/examsettingsdlg.cpp" line="81"/>
+        <source>Exam&apos;s level was changed
+and not saved !!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/exam/examsettingsdlg.cpp" line="96"/>
+        <source>Save exam&apos;s level</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/exam/examsettingsdlg.cpp" line="104"/>
+        <source>Cannot open file for writing
+%1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/exam/examsettingsdlg.cpp" line="120"/>
+        <source>Load exam&apos;s level</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/exam/examsettingsdlg.cpp" line="129"/>
+        <source>File: %1 
+ is not Nootka level file !!!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/exam/examsettingsdlg.cpp" line="138"/>
+        <source>Cannot open file for reading
+%1 </source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
