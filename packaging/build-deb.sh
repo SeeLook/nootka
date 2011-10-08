2daa128e6f606b468dc41b5dac204f83d26baeb4
SeeLook <945374+SeeLook@users.noreply.github.com>
2011-10-08 19:48:41 +0200

    deb

--- packaging/build-deb.sh
+++ packaging/build-deb.sh
@@ -11,26 +11,47 @@
 VERSION=$1
 BUILD_DIR=$2
 SRC_DIR=$3
-ARCH=$(uname -m)
+ARCH=$(dpkg-architecture -qDEB_BUILD_ARCH)
 
 printf "\033[01;35mBuilding Debian binary package for \033[01;32mnootka-$VERSION-$ARCH\n" 
 printf "\033[01;00m"
 
-# echo 
-# DEB_TOOLS=0
-# if [ $(whereis fakeroot|grep bin) ]; then
+echo " - Searching for tools for building deb package..."
+	DEB_TOOLS=1
+	if [ "$(whereis fakeroot|grep bin)" != "" ]; then
+		printf "\tfakeroot found\n"
+	else
+		printf "\tfakeroot not found\n"
+		DEB_TOOLS=0
+	fi
+	if [ "$(whereis dpkg-deb|grep bin)" != "" ]; then
+		printf "\tdpkg-deb found\n"
+	else
+		printf "\tdpkg-deb not found\n"
+		DEB_TOOLS=0
+	fi
+	if [ $DEB_TOOLS == 0 ]; then
+	printf "\033[01;31mInstall build-esentiall and fakeroot first !!!\033[01;00m\n"
+	fi
 
-
+#############################################################################################
+####  COPYING FILES              ############################################################
+#############################################################################################
 if [ -f $BUILD_DIR/src/nootka ]; then
-  # Cleaning previous 
-  if [ -d $BUILD_DIR/debian ]; then
-    echo "cleaning...."
-    rm -rf $BUILD_DIR/debian
-  fi
-  # Building dir tree
-  mkdir -p $BUILD_DIR/debian/DEBIAN
-  mkdir -p $BUILD_DIR/debian/usr/bin
-  mkdir -p $BUILD_DIR/debian/usr/share/nootka/doc/nootka
+	# Cleaning previous 
+	if [ -d $BUILD_DIR/debian ]; then
+		echo " - cleaning..."
+		rm -rf $BUILD_DIR/debian
+	fi
+	# Building dir tree
+	mkdir -p $BUILD_DIR/debian/DEBIAN
+	mkdir -p $BUILD_DIR/debian/usr/bin
+	mkdir -p $BUILD_DIR/debian/usr/share/nootka/doc/nootka
+	mkdir -p $BUILD_DIR/debian/usr/share/nootka/mime/packages
+
+	echo " - copying files..."
+	cp $BUILD_DIR/src/nootka $BUILD_DIR/debian/usr/bin/
+	$SRC_DIR/packaging/debian/control.sh $VERSION $ARCH > $BUILD_DIR/debian/DEBIAN/control
 
 
 
