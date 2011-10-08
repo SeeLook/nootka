201cab69c23ee950b9a4035eb0abeec61b24f9a2
Tomasz Bojczuk <tomaszbojczuk@gmail.com>
2011-10-08 14:04:01 +0200

    build deb

--- packaging/build-deb.sh
+++ packaging/build-deb.sh
@@ -1,9 +1,19 @@
 #! /bin/bash
-
+# Script for building Nootka debian biniary package
+# It ussualy is invoked by make deb
+# USAGE:
+# build-deb.sh version build-directory source-directory
+# 
+# Author:
+#           Tomasz Bojczuk <tomaszbojczuk@gmail.com>
+#
 
 VERSION=$1
 BUILD_DIR=$2
 SRC_DIR=$3
+ARCH=$(uname -m)
 
-printf "\033[01;35mBuilding debian binary package for \033[01;32mnootka-$VERSION\n" 
+printf "\033[01;35mBuilding Debian binary package for \033[01;32mnootka-$VERSION-$ARCH\n" 
 printf "\033[01;00m"
+
+mkdir -p 
