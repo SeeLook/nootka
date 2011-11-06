900e28a95878a06bca09ea69048230ca5865454e
SeeLook <945374+SeeLook@users.noreply.github.com>
2011-11-06 10:09:18 +0100

    Texam

--- src/exam/texam.h
+++ src/exam/texam.h
@@ -1,29 +1,27 @@
 /***************************************************************************
-*   Copyright (C) 2011 by Tomasz Bojczuk                                  *
-*   tomaszbojczuk@gmail.com                                               *
-*                                                                         *
-*   This program is free software;
-you can redistribute it and/or modify  *
-*   it under the terms of the GNU General Public License as published by  *
-*   the Free Software Foundation;
-either version 3 of the License, or     *
-*   (at your option) any later version.                                   *
-*                                                                         *
-*   This program is distributed in the hope that it will be useful,       *
-*   but WITHOUT ANY WARRANTY;
-without even the implied warranty of        *
-*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
-*   GNU General Public License for more details.                          *
-*                                                                         *
-*  You should have received a copy of the GNU General Public License      *
-*  along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
-***************************************************************************/
+ *   Copyright (C) 2011 by Tomasz Bojczuk  				   *
+ *   tomaszbojczuk@gmail.com   						   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License	   *
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
+ ***************************************************************************/
 
 #ifndef TEXAM_H
 #define TEXAM_H
 #include <QString>
 #include <QList>
 
+class Ttune;
 class TQAunit;
 class Tlevel;
 
@@ -37,22 +35,30 @@ class Texam
 
 public:
     explicit Texam();
-	Texam(Tlevel l);
+	Texam(Tlevel *l);
 	Texam(QString examFile);
     virtual ~Texam();
 	
-	bool loadExamFromFile(QString &f);
-	bool saveExamToFile();
+	Tlevel* level() { return m_level; }
+	void setLevel(Tlevel *l);
+	
+	void addQuestion(TQAunit &question) { m_answList << question; }
+	void setAnswer(TQAunit &answer) { m_answList.last() = answer; }
+	TQAunit curQ() { return m_answList.last(); }
 	
 	
+	bool loadExamFromFile(QString &fileName);
+	bool saveExamToFile(QString &fileName = 0);
+	
+	  /** Magic number in exam file to identify it.*/
 	static const qint32 examVersion;
 	
 	
 private:
-	QString m_fileName;
-	Tlevel m_level;
+	QString m_fileName, m_userName;
+	Tlevel *m_level;
 	QList<TQAunit> m_answList;
-		
+	Ttune m_tune;		
 		
 		
 		
