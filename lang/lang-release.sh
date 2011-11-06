1706bf6b14470800298b823fd70eabe1abe20f85
SeeLook <945374+SeeLook@users.noreply.github.com>
2011-11-06 16:48:29 +0100

    lang update scripts

--- lang/lang-release.sh
+++ lang/lang-release.sh
@@ -3,15 +3,14 @@
 #	script to update all *.ts in lang dir
 #
 
-SRC_DIR="../src"
 
 
-if [ ! -f "$SRC_DIR/main.cpp" ]; then 
+if [ ! -f  nootka_pl.ts ]; then 
 	echo "Run script into lang dir !!!!"
 	exit
 fi
 
 for TS in $(ls *.ts); do
-	echo $TS
-	lupdate -recursive $SRC_DIR -ts $TS
+	echo ${TS%.*}
+	lrelease ${TS%.*}.ts -qm ${TS%.*}.qm
 done
