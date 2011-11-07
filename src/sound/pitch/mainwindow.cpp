d26736c0eb19840d215fe8669540b8f3c237d781
Seemion Seemionych <seelook@gmail.com>
2011-11-07 09:33:10 +0100

    pitchFinder - creating test files

---
+++ src/sound/pitch/mainwindow.cpp
@@ -0,0 +1,70 @@
+/***************************************************************************
+ *   Copyright (C) 2011 by Tomasz Bojczuk  				   *
+ *   tomaszbojczuk@gmail.com   						   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License	   *
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
+ ***************************************************************************/
+
+#include "mainwindow.h"
+#include <QtGui>
+
+// #include "tnote.h"
+
+
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent)
+{
+
+    QWidget *widget = new QWidget(this);
+
+    QVBoxLayout *mLay = new QVBoxLayout;
+
+    devListCombo = new QComboBox(widget);
+    mLay->addWidget(devListCombo);
+    devNameLab = new QLabel(widget);
+    mLay->addWidget(devNameLab);
+	startSniffBut = new QPushButton("start sniffinfg", widget);
+	mLay->addWidget(startSniffBut);
+	QHBoxLayout *peakLay = new QHBoxLayout;
+	QLabel *peakDescLab = new QLabel("max peak: ", widget);
+	peakLay->addWidget(peakDescLab);
+	peakLab = new QLabel(widget);
+	peakLay->addWidget(peakLab);
+	mLay->addLayout(peakLay);
+	mLay->addStretch(1);
+	QHBoxLayout *pitchLay = new QHBoxLayout;
+	QLabel *pitchDescLab = new QLabel("pitch: ", widget);
+	pitchLay->addWidget(pitchDescLab);
+	pitchLab = new QLabel(widget);
+	pitchLay->addWidget(pitchLab);
+	mLay->addLayout(pitchLay);
+
+    widget->setLayout(mLay);
+    setCentralWidget(widget);
+	
+	m_levelTimer = new QTimer();
+
+//     devListCombo->addItems(Tengine::getAudioDevicesList());
+
+    connect(devListCombo, SIGNAL(currentIndexChanged(QString)), this, SLOT(setDeviceName(QString)));
+	connect(startSniffBut, SIGNAL(clicked(bool)), this, SLOT(startSniffSlot()));
+	connect(m_levelTimer, SIGNAL(timeout()), this, SLOT(levelSlot()));
+
+}
+
+void MainWindow::setDeviceName(QString devN) {
+    devNameLab->setText(devN);
+// 	m_audioEngine->setAudioDevice(devN);
+}
---
+++ src/sound/pitch/mainwindow.h
@@ -0,0 +1,53 @@
+/***************************************************************************
+ *   Copyright (C) 2011 by Tomasz Bojczuk  				   *
+ *   tomaszbojczuk@gmail.com   						   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License	   *
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
+ ***************************************************************************/
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+class QComboBox;
+class QLabel;
+class QPushButton;
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+public:
+    explicit MainWindow(QWidget *parent = 0);
+
+    QString deviceName() { return m_devName; }
+
+
+signals:
+
+public slots:
+    void setDeviceName(QString devN);
+
+private:
+    QComboBox *devListCombo;
+    QLabel *devNameLab, *peakLab, *pitchLab;
+	QPushButton *startSniffBut;
+	
+
+    QString m_devName;
+	QTimer *m_levelTimer;
+
+
+};
+
+#endif // MAINWINDOW_H
