79f8df49013b583721a1bc247ad988be43223bb2
Seemion Seemionych <seelook@gmail.com>
2011-12-06 09:19:04 +0100

    fixing bugs after divideing TexamExecutor. Works fine

--- src/exam/texam.cpp
+++ src/exam/texam.cpp
@@ -23,70 +23,67 @@
 #include <QFile>
 #include <QDataStream>
 #include <QMessageBox>
+// #include <QDebug>
 
 /*static*/
 const qint32 Texam::examVersion = 0x95121702;
 
 
-Texam::Texam():
-	m_level(0)	
-{
-
-}
 
 Texam::Texam(TexamLevel* l, QString userName):
 	m_level(l),
-	m_userName(userName)
+	m_userName(userName),
+	m_fileName(""),
+	m_mistNr(0)
 {
 
 }
 
 
 Texam::~Texam()
-{
-  delete m_level;
-}
+{}
 
 
 Texam::EerrorType Texam::loadFromFile(QString& fileName) {
 	m_fileName = fileName;
 	QFile file(fileName);
-//     quint16 questNr, mistNr, tmpAverTime, averTime = 0;
 	quint16 questNr;
 	m_workTime = 0;
+  m_mistNr = 0;
 	EerrorType result = e_file_OK;
     if (file.open(QIODevice::ReadOnly)) {
-				 QDataStream in(&file);
-         in.setVersion(QDataStream::Qt_4_7);
-         quint32 ev; //exam template version
-         in >> ev;
-         if (ev != examVersion)
-             return e_file_not_valid;
-		 
-         in >> m_userName;
-         getLevelFromStream(in, *(m_level));
-         in >> m_tune;
-         in >> m_totalTime;
-         in >> questNr >> m_averReactTime >> m_mistNr;
-         bool isExamFileOk = true;
-         int tmpMist = 0;
-         while (!in.atEnd()) {
-             TQAunit qaUnit;
-             if (!getTQAunitFromStream(in, qaUnit))
-                 isExamFileOk = false;
-                 m_answList << qaUnit;
-                 m_workTime += qaUnit.time; // 
-                 if ( !qaUnit.correct() )
-                     tmpMist++;
+      QDataStream in(&file);
+      in.setVersion(QDataStream::Qt_4_7);
+      quint32 ev; //exam template version
+      in >> ev;
+      if (ev != examVersion)
+          return e_file_not_valid;
+
+      in >> m_userName;
+      getLevelFromStream(in, *(m_level));
+      in >> m_tune;
+      in >> m_totalTime;
+      in >> questNr >> m_averReactTime >> m_mistNr;
+      bool isExamFileOk = true;
+      int tmpMist = 0;
+      while (!in.atEnd()) {
+          TQAunit qaUnit;
+          if (!getTQAunitFromStream(in, qaUnit))
+              isExamFileOk = false;
+          m_answList << qaUnit;
+          m_workTime += qaUnit.time; // 
+          if ( !qaUnit.correct() )
+              tmpMist++;
 //                     qDebug() << isExamFileOk << m_answList.size() <<
 //                             (int)qaUnit.key.value() << qaUnit.key.isMinor();
-                 }
-                 if (tmpMist != m_mistNr || questNr != m_answList.size()) {
-                   isExamFileOk = false;
-                   m_mistNr = tmpMist; //we try to fix exam file to give proper number of mistakes
-                 }
-                 if (!isExamFileOk)
-                     result = e_file_corrupted;        
+          }
+          if (tmpMist != m_mistNr || questNr != m_answList.size()) {
+            isExamFileOk = false;
+//             qDebug() << "mistakes:" << tmpMist << m_mistNr << "questions:" << questNr << m_answList.size();
+            m_mistNr = tmpMist; //we try to fix exam file to give proper number of mistakes
+          }
+          if (!isExamFileOk)
+              result = e_file_corrupted;        
      } else {
          TlevelSelector::fileIOerrorMsg(file, 0);
 				 result = e_cant_open;
