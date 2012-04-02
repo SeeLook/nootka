55b7d6105a7ddb5a8bd86a8dcbbf032fb0a78ac5
Tomasz Bojczuk <tomaszbojczuk@gmail.com>
2012-04-02 12:31:42 +0200

    mac qapp

---
+++ src/qappmac.h
@@ -0,0 +1,41 @@
+/***************************************************************************
+ *   Copyright (C) 2012 by Tomasz Bojczuk                                  *
+ *   tomaszbojczuk@gmail.com                                               *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License	   *
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
+ ***************************************************************************/
+
+#ifndef QAPPMAC_H
+#define QAPPMAC_H
+
+
+#include <QtGui>
+
+
+
+class QAppMac : public QApplication
+{
+	Q_OBJECT
+public:
+	QAppMac(int & argc, char **argv);
+// 	virtual ~QAppMac();
+	
+public signals:
+	void fileToOpen(QString fileName);
+	
+protected:
+        bool event(QEvent *);
+
+};
+#endif 
