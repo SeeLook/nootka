499814caac285e0ca51bea91b68ff7eac9e8a7bc
SeeLook <945374+SeeLook@users.noreply.github.com>
2012-04-27 13:13:21 +0000

    tquestionpoint

---
+++ src/analyser/tquestionpoint.h
@@ -0,0 +1,47 @@
+/***************************************************************************
+ *   Copyright (C) 2012 by Tomasz Bojczuk      	                	   	   *
+ *   tomaszbojczuk@gmail.com   						                       *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License	   *
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
+ ***************************************************************************/
+
+#ifndef TQUESTIONPOINT_H
+#define TQUESTIONPOINT_H
+
+#include <QGraphicsItem>
+#include <QFont>
+
+
+class TquestionPoint : public QGraphicsItem
+{
+
+public:
+  TquestionPoint(TQAunit *question);
+  virtual ~TquestionPoint() {}
+  
+  virtual void paint(QPainter* painter, const QStyleOptionGraphicsItem* option, QWidget* widget = 0);
+  virtual QRectF boundingRect() const;
+  TQAunit* question() { return m_question; } 
+  
+  static void setColors(QColor goodColor = Qt::green, QColor wrongColor = Qt::red, QColor notBadColor = Qt::yelow);
+  
+  
+private:
+  TQAunit *m_question;
+  
+  static QColor m_goodColor, m_wrongColor, m_notBadColor;
+  
+};
+
+#endif // TQUESTIONPOINT_H
