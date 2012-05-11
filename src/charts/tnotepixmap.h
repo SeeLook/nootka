03b3b9474d58be7613da5ceda07c3cc250b38281
SeeLook <945374+SeeLook@users.noreply.github.com>
2012-05-11 12:43:01 +0200

    TnotePixmap

---
+++ src/charts/tnotepixmap.h
@@ -0,0 +1,36 @@
+/***************************************************************************
+ *   Copyright (C) 2012 by Tomasz Bojczuk                                  *
+ *   tomaszbojczuk@gmail.com                                               *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
+ ***************************************************************************/
+
+
+#ifndef TNOTEPIXMAP_H
+#define TNOTEPIXMAP_H
+
+#include <QPixmap>
+#include <tnote.h>
+#include <tkeysignature.h>
+
+
+class TnotePixmap : public QPixmap
+{
+
+public:
+    TnotePixmap(Tnote note, int height = 100, TkeySignature key = TkeySignature(0));
+    virtual ~TnotePixmap();
+};
+
+#endif // TNOTEPIXMAP_H
