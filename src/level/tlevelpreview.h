a6f5626728803731564dbda37327bc672598b5a9
Seemion Seemionych <seelook@gmail.com>
2012-09-11 09:43:06 +0200

    ...

---
+++ src/level/tlevelpreview.h
@@ -0,0 +1,45 @@
+/***************************************************************************
+ *   Copyright (C) 2012 by Tomasz Bojczuk                                  *
+ *   tomaszbojczuk@gmail.com                                               *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
+ ***************************************************************************/
+
+#ifndef TLEVELPREVIEW_H
+#define TLEVELPREVIEW_H
+
+#include <QWidget>
+
+class TexamLevel;
+class QLabel;
+
+class TlevelPreview : public QWidget
+{
+
+public:
+  explicit TlevelPreview(QWidget *parent = 0);
+  virtual ~TlevelPreview();
+  
+  static QString notesRangeTxt() { return tr("notes' range:"); }
+  static QString fretsRangeTxt() { return tr("frets' range:"); }
+
+  void setLevel(TexamLevel
+  &tl);
+  
+private:
+  QLabel *summLab;
+
+};
+
+#endif // TLEVELPREVIEW_H
