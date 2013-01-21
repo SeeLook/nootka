13d8e394a682644a1b37e958d12ec4326a511074
Tomasz Bojczuk <tomaszbojczuk@gmail.com>
2013-01-21 19:59:34 +0100

    missed class...

---
+++ src/charts/ttiphandler.cpp
@@ -0,0 +1,97 @@
+/***************************************************************************
+ *   Copyright (C) 2013 by Tomasz Bojczuk                                  *
+ *   tomaszbojczuk@gmail.com                                               *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
+ ***************************************************************************/
+
+
+#include "ttiphandler.h"
+#include "tgraphicstexttip.h"
+#include "tstatisticstip.h"
+
+#include <QGraphicsSceneHoverEvent>
+#include <QGraphicsScene>
+#include <QGraphicsView>
+#include <QTimer>
+
+
+/*static*/
+TgraphicsTextTip* TtipHandler::tip = 0;
+QTimer* TtipHandler::m_delTimer = 0;
+
+
+TtipHandler::TtipHandler()
+{
+  setAcceptHoverEvents(true);
+  if (!m_delTimer) {
+    m_delTimer = new QTimer();
+    connect(m_delTimer, SIGNAL(timeout()), this, SLOT(delayedDelete()));
+  }
+}
+
+TtipHandler::~TtipHandler() {
+  if (m_delTimer) {
+    delete m_delTimer;
+    m_delTimer = 0;
+  }
+  if (tip) {
+    delete tip;
+    tip = 0;
+  }
+}
+
+
+
+
+void TtipHandler::handleTip(QPointF scenePos) {
+    if (m_delTimer->isActive()) {
+      m_delTimer->stop();
+    }
+    if (tip) {
+      scene()->addItem(tip);
+      tip->setFlag(QGraphicsItem::ItemIgnoresTransformations);
+      QSize s = scene()->views()[0]->size();
+      QPointF mapedP = scene()->views()[0]->mapFromScene(scenePos);
+//       determine where to display tip when point is near a view boundaries
+      if (mapedP.x() > (s.width() / 2) ) // tip on the left
+          scenePos.setX(scenePos.x() - tip->boundingRect().width() / scene()->views()[0]->transform().m11());
+      if (mapedP.y() > (s.height() / 2) )
+         scenePos.setY(scenePos.y() - tip->boundingRect().height() / scene()->views()[0]->transform().m22());
+      tip->setPos(scenePos);
+      tip->setZValue(70);
+      update();
+    }
+}
+
+
+void TtipHandler::hoverLeaveEvent(QGraphicsSceneHoverEvent*) {
+    if (!m_delTimer->isActive()) {
+        m_delTimer->start(20);
+    }
+}
+
+void TtipHandler::delayedDelete() {
+    if (isUnderMouse() || (tip && tip->isUnderMouse()))
+        return;
+    m_delTimer->stop();
+    if (tip) {
+        delete tip;
+        tip = 0;
+        update();
+        scene()->update();
+    }
+}
+
+
