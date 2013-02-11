63e92830e28982c120dd058d63b0d5f054b4461a
SeeLook <945374+SeeLook@users.noreply.github.com>
2013-02-11 08:48:08 +0100

    ...

--- packaging/make-chlog.sh
+++ packaging/make-chlog.sh
@@ -22,7 +22,7 @@ printf "Nootka ($VERSION) all-deb; urgency=low\n" > $DEST_FILE
 echo "
   * Changes set:
 " >> $DEST_FILE
-$SRC_DIR/packaging/getLastChangesSet.sh $SRC_DIR/changelog >> $DEST_FILE
+$SRC_DIR/packaging/getLastChangesSet.sh $SRC_DIR/changes >> $DEST_FILE
 # echo " -- See Look <seelook@gmail.com>  $(date +%G-%m-%d)" >> $DEST_FILE
 echo "
  -- See Look <seelook@gmail.com>  $(date -R)" >> $DEST_FILE
