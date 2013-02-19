3b0df5f243b0905c2c9b92cac93ccf3a02d4579b
SeeLook <945374+SeeLook@users.noreply.github.com>
2013-02-19 18:18:46 +0100

    ...

--- src/sound/pulseprober.cpp
+++ src/sound/pulseprober.cpp
@@ -59,15 +59,16 @@ bool hasPulsePlugin() {
 
 bool checkForPulse() {
     if (hasPulsePlugin()) {
-      qDebug() << "QT complied with pulse.\nChecking is pulseaudio running...";
-      QProcess *ps = new QProcess();
-      QStringList args;
-      args << "pulse";
-      ps->start("pgrep", args);
-      ps->waitForFinished();
-      QString exitText(ps->readAllStandardOutput());
-      delete ps;
-      if (exitText != "")  
+      qDebug() << "QT complied with pulse.\nChecking is pulseaudio available...";
+//       QProcess *ps = new QProcess();
+//       QStringList args;
+//       args << "pulse";
+//       ps->start("pgrep", args);
+//       ps->waitForFinished();
+//       QString exitText(ps->readAllStandardOutput());
+//       delete ps;
+      QFileInfo pulseBin("/usr/bin/pulseaudio");
+      if (pulseBin.exists())  
         return true;
       else {
         qDebug() << "PULSEAUDIO IS NOT RUNNING !!!!!!";
