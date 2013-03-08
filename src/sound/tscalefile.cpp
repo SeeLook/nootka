727e98c0591c89abaf42e71a05c13d610a5b38ce
SeeLook <945374+SeeLook@users.noreply.github.com>
2013-03-08 08:12:09 +0100

    excluded loading wav file to separate class

--- src/sound/tscalefile.cpp
+++ src/sound/tscalefile.cpp
@@ -18,14 +18,64 @@
 
 
 #include "tscalefile.h"
+#include <QFile>
+#include <QDataStream>
+#include <QDebug>
 
-TscaleFile::TscaleFile()
-{
-
-}
+TscaleFile::TscaleFile() :
+  m_audioArr(0)
+{}
 
 TscaleFile::~TscaleFile()
 {
-
+    deleteData();
 }
+
+
+bool TscaleFile::loadAudioData(QString& path) {
+  QFile wavFile(path);
+  if (!wavFile.exists())
+      return false;
+  wavFile.open(QIODevice::ReadOnly);
+  QDataStream wavStream(&wavFile);
+  
+  int fmtSize;
+  char *chunkName = new char[4];
+  wavStream.skipRawData(16);
+  wavStream.readRawData(chunkName, 4);
+  fmtSize = *((int*)chunkName);
+  wavStream.readRawData(chunkName, 2);
+  short wavFormat = *((short*)chunkName);
+  
+  quint32 dataSizeFromChunk;
+  wavStream.readRawData(chunkName, 2);
+  unsigned short m_chanels = *((unsigned short*)chunkName);
+  wavStream.readRawData(chunkName, 4);
+  quint32 sampleRate = *((quint32*)chunkName);
+  wavStream.skipRawData(fmtSize - 8 + 4);
+  wavStream.readRawData(chunkName, 4);
+  dataSizeFromChunk = *((quint32*)chunkName);
+//   qDebug() << "data size: " << dataSizeFromChunk << 4740768;
+  // we check is wav file this proper one ? 4740766
+  if (m_chanels != 1 || wavFormat != 1 || sampleRate != 22050 || dataSizeFromChunk != 4740766) {
+      qDebug() << "wav file error occured " << dataSizeFromChunk << m_chanels
+              << wavFormat << sampleRate;
+      return false;
+  }
+  
+  m_audioArr = new qint16[dataSizeFromChunk / 2];
+  wavStream.readRawData((char*)m_audioArr, dataSizeFromChunk);
+  wavFile.close();
+  return true;
+}
+
+
+void TscaleFile::deleteData() {
+  if (m_audioArr) {
+    delete m_audioArr;
+    m_audioArr = 0;
+  }
+}
+
+
 
