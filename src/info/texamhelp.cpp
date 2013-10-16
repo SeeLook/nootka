a7695cd39038c88b59630d7885ded660f5287cd9
SeeLook <seelook@gmail.com>
2013-10-16 10:33:08 +0200

    start something help finished, exam/exercise settings done

--- src/info/texamhelp.cpp
+++ src/info/texamhelp.cpp
@@ -26,39 +26,41 @@
 
 
 #define PIXICONSIZE (32)
+#define BIGPIXSIZE (64)
 
 TexamHelp::TexamHelp(QString questColorTxt, QString answColorTxt, bool* showHelp, QWidget* parent) :
     ThelpDialogBase(parent, 0)
 {
-  resize((parent->width()/3)*2, (parent->height()/3)*2);
-  setWindowTitle(tr("Exam help"));
+  resize((parent->width() / 3) * 2, (parent->height() / 3) * 2);
+//   setWindowTitle(tr("Exam help"));
   helpText()->setHtml(
-    QString("<center><img src=\"%1\"><span style=\"font-size: 18px;\"> ").arg(path()+"picts/help.png") +
-    tr("How does an exam work ?") +
-    QString(" </span><img src=\"%1\"> ").arg(path() + "picts/startExam.png") + "<br><br>" +
-    "<style type=\"text/css\">img { border-style: solid; border-color: palette(text); border-width: 2px; background-color: palette(window); }</style>" +
-    toGetQuestTxt() + ":<br>- " + clickSomeButtonTxt(pixToHtml(path() + "picts/nextQuest.png", PIXICONSIZE)) + "<br>- " +
+    QString("<center><h2>%1 &nbsp;&nbsp;").arg(pix("help", BIGPIXSIZE)) +
+    tr("How does an exercise or an exam work?") +
+    QString("&nbsp;&nbsp; %1").arg(pix("startExam", BIGPIXSIZE)) + "</h2><br>" +
+//     "<style type=\"text/css\">img { border-style: solid; border-color: palette(text); border-width: 2px; background-color: palette(window); }</style>" +
+    toGetQuestTxt() + ":<br>- " + clickSomeButtonTxt(pix("nextQuest", PIXICONSIZE)) + "<br>- " +
     pressSpaceKey() + "<br>- " + orRightButtTxt() + "<br>" + 
     tr("Select 2nd check box to get the question automatically.") + 
     QString("<br><br><span style=\"%1\">").arg(questColorTxt) +
     tr("Questions are marked with this color and \"?\" mark.") + "</span><br>" + 
     tr("To give an answer, select it on <span style=\"%1\">Nootka's element with that color.</span><br>")
       .arg(answColorTxt) +
-    QString("<br><br><img src=\"%1\"><br><br>").arg(path()+"picts/scr.png") +
+    QString("<br><br>%1<br><br>").arg(pix("scr", 0)) +
     tr("To check the answer confirm it:") + "<br>- " + 
-    clickSomeButtonTxt(pixToHtml(path() + "picts/check.png", PIXICONSIZE)) + "<br>- " +
+    clickSomeButtonTxt(pix("check", PIXICONSIZE)) + "<br>- " +
     pressEnterKey() + "<br>- " + orRightButtTxt() + "<br><br>" +
     tr("If you made a mistake and you want to repeat the question:") + "<br>- " + 
-    clickSomeButtonTxt(pixToHtml(path()+"picts/prevQuest.png", PIXICONSIZE)) + "<br>- " + orPressBkSTxt() + "<br>" +
+    clickSomeButtonTxt(pix("prevQuest", PIXICONSIZE)) + "<br>- " + orPressBkSTxt() + "<br>" +
     tr("By selecting the 3rd check box, answers will be checked immediately without confirmation required.") + "<br><br>" +
-    toStopExamTxt(pixToHtml(path() + "picts/stopExam.png", PIXICONSIZE)) + "<br><br>" + 
+    toStopExamTxt(pix("stopExam", PIXICONSIZE)) + "<br><br>" + 
     tr("Exam will be complete when you give answers to all questions.<br>The number of questions depends on the level of the exam.") + "<br>" +
     tr("If you make a mistake, the question will asked two more times and the total number of questions will be increased. These are penalty questions.") + "<br>" +
     tr("When answer is 'not bad', you get one penalty more.") +
     "<br><br><span style=\"font-size: 20px;\"><b>" +
     tr("GOOD LUCK!") + "</b></span>" + "<br><hr><table><tr><th colspan=2>" +
     tr("Experts corner") + "</th></tr><tr><td rowspan=3>" +
-    QString("<img style=\"background-color: transparent;\" src=\"%1\">").arg(path()+"picts/expertCorner.png") +
+//     QString("<img style=\"background-color: transparent;\" src=\"%1\">").arg(pix("expertCorner"))+
+		pix("expertCorner", 0) +
     "</td><td><br>1. " + TexamSettings::correctMistakesTxt() + "</td></tr><tr><td><br>2. " + 
     TexamSettings::autoNextQuestTxt() + "</td></tr><tr><td><br>3. " + 
     TexamSettings::expertsAnswerTxt() + "</td></tr></table>");
--- src/settings/texamsettings.cpp
+++ src/settings/texamsettings.cpp
@@ -21,6 +21,7 @@
 #include "tcolorbutton.h"
 #include "texamparams.h"
 #include "texpertanswerhelp.h"
+#include <troundedlabel.h>
 #include <QtGui>
 
 
@@ -31,54 +32,91 @@ TexamSettings::TexamSettings(TexamParams* params, QColor* qColor, QColor* aColor
     m_aColor(aColor),
     m_nbColor(nbColor)
 {
-    QVBoxLayout *lay = new QVBoxLayout;
+		m_correctChB = new QCheckBox(tr("correct wrong answers"), this);
+			m_correctChB->setStatusTip(tr("When you will make mistake, the program will show you how a correct answer should be."));
+		QLabel *timeLab = new QLabel(tr("duration preview of corrected answer"), this);
+		m_viewTimeSlider = new QSlider(Qt::Horizontal, this);
+			m_viewTimeSlider->setMinimum(2000);
+			m_viewTimeSlider->setMaximum(6000);
+			m_viewTimeSlider->setValue(m_params->correctViewDuration);
+			m_viewTimeSlider->setSingleStep(500);
+			m_viewTimeSlider->setPageStep(1000);
+			m_viewTimeSlider->setTickPosition(QSlider::TicksBothSides);
+			m_viewTimeSlider->setTickInterval(500);
+		m_timeLabel = new TroundedLabel(this);
+			timePreviewChanged(m_params->correctViewDuration);
+		m_suggestExamChB = new QCheckBox(tr("suggest an exam"), this);
+			m_suggestExamChB->setStatusTip(tr("Watch exercising progress and when exercising is going well, suggest to start an exam on exercise level."));
 
-    m_autoNextChB = new QCheckBox(autoNextQuestTxt(), this);
-    lay->addWidget(m_autoNextChB, 0, Qt::AlignCenter);
-    m_autoNextChB->setChecked(m_params->autoNextQuest);
+    
     m_repeatIncorChB = new QCheckBox(tr("repeat a question when its answer is incorrect"), this);
-    lay->addWidget(m_repeatIncorChB, 0, Qt::AlignCenter);
-    m_repeatIncorChB->setChecked(m_params->repeatIncorrect);
-    m_repeatIncorChB->setStatusTip(tr("A question with an incorrect answer will be asked once again."));
-    m_expertAnswChB = new QCheckBox(expertsAnswerTxt(), this);
-    m_expertAnswChB->setChecked(m_params->expertsAnswerEnable);
-    lay->addWidget(m_expertAnswChB, 0, Qt::AlignCenter);
-    lay->addStretch(1);
+			m_repeatIncorChB->setChecked(m_params->repeatIncorrect);
+			m_repeatIncorChB->setStatusTip(tr("A question with an incorrect answer will be asked once again."));
+		m_closeConfirmChB = new QCheckBox(tr("close without confirm"), this);
+			m_closeConfirmChB->setStatusTip(tr("If checked, an application will not ask to answer pending question just mark it as wrong, save an exam to file and close without any confirmation needed."));
     
-    
-    QHBoxLayout *nameLay = new QHBoxLayout();
-    QLabel *nameLab = new QLabel(tr("student's name:"), this);
-		nameLay->addStretch();
-    nameLay->addWidget(nameLab);
-		nameLay->addStretch();
+		m_autoNextChB = new QCheckBox(autoNextQuestTxt(), this);
+			m_autoNextChB->setChecked(m_params->autoNextQuest);
+		m_expertAnswChB = new QCheckBox(expertsAnswerTxt(), this);
+			m_expertAnswChB->setChecked(m_params->expertsAnswerEnable);    
+    QLabel *nameLab = new QLabel(tr("Student Name:"), this);
     m_nameEdit = new QLineEdit(m_params->studentName, this);
-    m_nameEdit->setMaxLength(30);
-    nameLay->addWidget(m_nameEdit);
-		nameLay->addStretch();
-    m_nameEdit->setStatusTip(tr("Default name for every new exam."));
-    lay->addLayout(nameLay);
-    lay->addStretch(1);
+			m_nameEdit->setMaxLength(30);
+			m_nameEdit->setStatusTip(tr("Default name for every new exam or exercise."));
 
-    QGridLayout *colLay = new QGridLayout;
     QLabel *questLab = new QLabel(tr("color of questions") + " / " + tr("color of wrong answers"), this);
     m_questColorBut = new TcolorButton(*(m_qColor), this);
-    colLay->addWidget(questLab, 0, 0, Qt::AlignRight);
-    colLay->addWidget(m_questColorBut, 0, 1, Qt::AlignLeft);
     QLabel *answLab = new QLabel(tr("color of answers") + " / " + tr("color of correct answers"), this);
     m_answColorBut = new TcolorButton(*(m_aColor), this);
-    colLay->addWidget(answLab, 1, 0, Qt::AlignRight);
-    colLay->addWidget(m_answColorBut, 1, 1, Qt::AlignLeft);
     QLabel *notBadLab = new QLabel(tr("color of 'not bad' answers"), this);
     m_notBadButt = new TcolorButton(*(m_nbColor), this);
-    colLay->addWidget(notBadLab, 2, 0, Qt::AlignRight);
-    colLay->addWidget(m_notBadButt, 2, 1, Qt::AlignLeft);
 
-    lay->addLayout(colLay);
-    lay->addStretch(1);
-
-    setLayout(lay);
+		
+		QVBoxLayout *mainLay = new QVBoxLayout;
+		QGroupBox *exerciseGr = new QGroupBox(tr("exercises"), this);
+		QVBoxLayout *exerciseLay = new QVBoxLayout;
+		QHBoxLayout *timeLay = new QHBoxLayout;
+			timeLay->addWidget(timeLab);
+			timeLay->addWidget(m_viewTimeSlider);
+			timeLay->addWidget(m_timeLabel);
+			exerciseLay->addWidget(m_correctChB, 0, Qt::AlignCenter);
+			exerciseLay->addLayout(timeLay);
+			exerciseLay->addWidget(m_suggestExamChB, 0, Qt::AlignCenter);
+		exerciseGr->setLayout(exerciseLay);
+		mainLay->addWidget(exerciseGr);
+		mainLay->addStretch();
+		QGroupBox *examGr = new QGroupBox(tr("exams"), this);
+		QVBoxLayout *examLay = new QVBoxLayout;
+			examLay->addWidget(m_repeatIncorChB);
+			examLay->addWidget(m_closeConfirmChB);
+		examGr->setLayout(examLay);
+		mainLay->addWidget(examGr);
+		mainLay->addStretch();
+		
+		mainLay->addWidget(m_autoNextChB, 0, Qt::AlignCenter);
+		mainLay->addWidget(m_expertAnswChB, 0, Qt::AlignCenter);
+		mainLay->addStretch();
+		QHBoxLayout *nameLay = new QHBoxLayout();
+			nameLay->addStretch();
+			nameLay->addWidget(nameLab);
+			nameLay->addStretch();
+			nameLay->addWidget(m_nameEdit);
+			nameLay->addStretch();
+		mainLay->addLayout(nameLay);
+			mainLay->addStretch();
+		QGridLayout *colorsLay = new QGridLayout;
+			colorsLay->addWidget(questLab, 0, 0, Qt::AlignCenter);
+			colorsLay->addWidget(m_questColorBut, 0, 1, Qt::AlignCenter);
+			colorsLay->addWidget(answLab, 1, 0, Qt::AlignCenter);
+			colorsLay->addWidget(m_answColorBut, 1, 1, Qt::AlignCenter);
+			colorsLay->addWidget(notBadLab, 2, 0, Qt::AlignCenter);
+			colorsLay->addWidget(m_notBadButt, 2, 1, Qt::AlignCenter);
+    mainLay->addLayout(colorsLay);
+    mainLay->addStretch();
+    setLayout(mainLay);
     
     connect(m_expertAnswChB, SIGNAL(clicked(bool)), this, SLOT(expertAnswersChanged(bool)));
+		connect(m_viewTimeSlider, SIGNAL(sliderMoved(int)), this, SLOT(timePreviewChanged(int)));
 }
 
 
@@ -87,6 +125,10 @@ void TexamSettings::saveSettings() {
     m_params->repeatIncorrect = m_repeatIncorChB->isChecked();
     m_params->expertsAnswerEnable = m_expertAnswChB->isChecked();
     m_params->studentName = m_nameEdit->text();
+		m_params->closeWithoutConfirm = m_closeConfirmChB->isChecked();
+		m_params->correctViewDuration = m_viewTimeSlider->value();
+		m_params->showCorrected = m_correctChB->isChecked();
+		m_params->suggestExam = m_suggestExamChB->isChecked();
         
     *m_qColor = m_questColorBut->getColor();
     m_qColor->setAlpha(40);
@@ -102,6 +144,10 @@ void TexamSettings::restoreDefaults() {
 		m_repeatIncorChB->setChecked(true);
 		m_expertAnswChB->setChecked(false);
 		m_nameEdit->setText("");
+		m_closeConfirmChB->setChecked(false);
+		m_viewTimeSlider->setValue(2000);
+		m_correctChB->setChecked(true);
+		m_suggestExamChB->setChecked(true);
 		m_questColorBut->setColor(QColor("red"));
 		m_answColorBut->setColor(QColor("green"));
 		m_notBadButt->setColor(QColor("#FF8000"));
@@ -115,3 +161,11 @@ void TexamSettings::expertAnswersChanged(bool enabled) {
         m_expertAnswChB->setChecked(false);
   }
 }
+
+
+void TexamSettings::timePreviewChanged(int val) {
+		m_timeLabel->setText(QString("%1 ms").arg(val));
+}
+
+
+
