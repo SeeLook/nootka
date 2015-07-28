eb0a8087c50e89b97bec62edf76607abc862fee6
SeeLook <945374+SeeLook@users.noreply.github.com>
2015-07-28 13:31:01 +0200

    Fixed validation of current position, updated documentation

--- src/libs/core/tfingerpos.h
+++ src/libs/core/tfingerpos.h
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2011-2014 by Tomasz Bojczuk                             *
+ *   Copyright (C) 2011-2015 by Tomasz Bojczuk                             *
  *   tomaszbojczuk@gmail.com                                               *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
@@ -26,53 +26,53 @@
 
 
 /**
-*  A class describing a finger position on a guitar fingerboard.
-* It supports up to six strings and up to 39 frets.
-* Default constructor @p TfingerPos() creates invalid position.
-* It can be detected with @p isValid() method
+ * A class describing a finger position on a guitar fingerboard.
+ * It supports up to six strings and up to 39 frets.
+ * Default constructor @p TfingerPos() creates invalid position.
+ * It can be detected with @p isValid() method
 */
 class NOOTKACORE_EXPORT TfingerPos
 {
 public:
-    TfingerPos() { m_pos = 255; }
-    TfingerPos(unsigned char realStr, unsigned char fret) {
-        setPos(realStr, fret);
-    }
+  TfingerPos() { m_pos = 255; }
+  TfingerPos(unsigned char realStr, unsigned char fret) {
+      setPos(realStr, fret);
+  }
 
-    quint8 str() { return (m_pos / 40) + 1; }
-    quint8 fret() { return m_pos % 40; }
+  quint8 str() { return (m_pos / 40) + 1; } /** Returns string number (real [1-6]) */
+  quint8 fret() { return m_pos % 40; } /** Returns fret number [0-40] */
 
-    bool isValid() { return m_pos < 201; } /** Returns @p True when position is valid */
-    
-    void setPos(unsigned char realStr, unsigned char fret) {
-        m_pos = (realStr - 1) * 40 + fret;
-    }
-    
-        /** List of QString with roman representation of numbers 0-24. */
-    static const QString fretsList[25];
-    static QString romanFret(quint8 fret);
-    QString romanFret() { return romanFret(fret()); }
-    
-    QString toHtml();  /** TfingerPos in HTML format as a string fe.: 3 XVII */
+  bool isValid() { return m_pos < 241; } /** Returns @p True when position is valid */
 
-    bool operator==( TfingerPos f2) { return m_pos == f2.m_pos; }
-    bool operator!=( TfingerPos f2) { return m_pos != f2.m_pos; }
+  void setPos(unsigned char realStr, unsigned char fret) {
+      m_pos = (realStr - 1) * 40 + fret;
+  }
 
-    friend QDataStream &operator<< (QDataStream &out, const TfingerPos &fPos) {
-        out << fPos.m_pos;
-        return out;
-    }
-    friend QDataStream &operator>> (QDataStream &in, TfingerPos &fPos) {
-        in >> fPos.m_pos;
-        return in;
-    }
-    
-				/** Writes XML structure with <string> and <fret> into <tag> (if not empty) */
-    void toXml(QXmlStreamWriter& xml, const QString& tag = "technical");
-		void fromXml(QXmlStreamReader& xml);
+  static const QString fretsList[25]; /** [STATIC] List of QString with roman representation of numbers 0-24. */
+  static QString romanFret(quint8 fret); /** [STATIC] Returns string with roman number of given value. Range [0-24] is supported */
+  QString romanFret() { return romanFret(fret()); } /** Returns string with roman number. Range [0-24] is supported */
 
-protected:
-    quint8 m_pos;
+  QString toHtml();  /** TfingerPos in HTML format as a string fe.: 3 XVII */
+
+  bool operator==( TfingerPos f2) { return m_pos == f2.m_pos; }
+  bool operator!=( TfingerPos f2) { return m_pos != f2.m_pos; }
+
+  friend QDataStream &operator<< (QDataStream &out, const TfingerPos &fPos) {
+      out << fPos.m_pos;
+      return out;
+  }
+
+  friend QDataStream &operator>> (QDataStream &in, TfingerPos &fPos) {
+      in >> fPos.m_pos;
+      return in;
+  }
+
+      /** Writes XML structure with <string> and <fret> wrapped into <tag> (if not empty) - by default it is <technical> */
+  void toXml(QXmlStreamWriter& xml, const QString& tag = "technical");
+  void fromXml(QXmlStreamReader& xml);
+
+  protected:
+  quint8 m_pos;
 };
 
 
