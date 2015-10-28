92576fe640c396bcc1ea5e691f4dee5c229b486d
SeeLook <seelook@gmail.com>
2015-10-28 23:09:20 +0100

    Tpath takes char* instead of QString for less memory and CPU usage, added lang() method with path to translations.

--- src/libs/core/tpath.cpp
+++ src/libs/core/tpath.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2014 by Tomasz Bojczuk                                  *
+ *   Copyright (C) 2014-2015 by Tomasz Bojczuk                             *
  *   tomaszbojczuk@gmail.com                                               *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
@@ -19,5 +19,5 @@
 
 #include "tpath.h"
 
-QString Tpath::main = "";
+QString Tpath::main = QString();
 
