06c71936efe74652de1d256b9310b7aa18e049e9
SeeLook <seelook@gmail.com>
2015-11-01 20:40:41 +0100

    New TfileDialog for Mobile and calling it selector, creator and starter.

--- src/libs/core/widgets/tfiledialog.h
+++ src/libs/core/widgets/tfiledialog.h
@@ -1,37 +1,73 @@
-/***************************************************************************
- *   Copyright (C) 2015 by Tomasz Bojczuk                                  *
- *   tomaszbojczuk@gmail.com                                               *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 3 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *  You should have received a copy of the GNU General Public License      *
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
- ***************************************************************************/
-
 #ifndef TFILEDIALOG_H
 #define TFILEDIALOG_H
 
-#include <QtWidgets/qfiledialog.h>
+#include <QtWidgets/qdialog.h>
+#include <QtWidgets/qfilesystemmodel.h>
+#include <QtWidgets/qboxlayout.h>
+
+
+class QListWidget;
+class QListWidgetItem;
+class QListView;
+class QLineEdit;
+class QLabel;
+class QComboBox;
 
 
 /**
- *
+ * Implementation of file dialog for mobile devices.
+ * It is far from ideal but much better than native QFileDialog.
+ * It has two static methods @p getOpenFileName and @p getSaveFileName
+ * with three parameters (in contrary to native there is no caption).
+ * Third @param filter may support multiple filters but only single one has been tested so far.
+ * NOTICE: In contrary to @class QFileDialog @param filter takes:
+ * only extension name WITHOUT any dots, parenthesis, asterisks or so.
+ * Multiple extensions can should be separated by |
+ * noo|nel or bz|gz|bz2
  */
-class TfileDialog : public QFileDialog
+class TfileDialog : public QDialog
 {
-public:
-  TfileDialog(QWidget* parent, const QString& caption, const QString& directory, const QString& filter);
+  Q_OBJECT
 
-  static QString getSaveFileName(QWidget* parent, const QString& caption, const QString& directory, const QString& filter);
-  static QString getOpenFileName(QWidget* parent, const QString& caption, const QString& directory, const QString& filter);
+public:
+  enum EacceptMode { e_acceptSave, e_acceptOpen }; /** File dialog type: for opening or saving */
+  Q_ENUM(EacceptMode)
+
+  explicit TfileDialog(QWidget* parent, const QString& directory, const QString& filter, EacceptMode mode);
+  ~TfileDialog();
+
+  static QString getSaveFileName(QWidget* parent = 0, const QString& directory = QString(), const QString& filter = QString());
+  static QString getOpenFileName(QWidget* parent = 0, const QString& directory = QString(), const QString& filter = QString());
+
+  QString selectedFile() const { return m_selectedFile; }
+
+  void setDirection(QBoxLayout::Direction dir) { m_lay->setDirection(dir); } /** Left to right by default */
+  EacceptMode acceptMode() { return m_acceptMode; }
+
+
+protected:
+  void itemClickedSlot(const QModelIndex& index);
+  void menuClickedSlot(QListWidgetItem* item);
+  void dirUpSlot();
+  void newDirSlot();
+  void performAction(); /** Open or save */
+
+  QListWidgetItem* addMenuItem(const QIcon& icon, const QString& text = QString());
+  void updateLocationLabel();
+
+private:
+  QFileSystemModel        *m_fileModel;
+  QString                  m_selectedFile;
+  EacceptMode              m_acceptMode;
+
+  QBoxLayout              *m_lay;
+  QListView               *m_list;
+  QListWidget             *m_menu;
+  QListWidgetItem         *m_acceptItem, *m_cancelItem, *m_dirUpItem, *m_newDirItem;
+  QLineEdit               *m_editName;
+  QLabel                  *m_locationLab;
+  QComboBox               *m_extensionCombo;
+
 };
 
 #endif // TFILEDIALOG_H
