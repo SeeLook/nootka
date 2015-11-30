c75df84aef4c15deb7c0f0c99d139554d2482b1e
SeeLook <945374+SeeLook@users.noreply.github.com>
2015-11-30 09:42:01 +0100

    Main help has icon size related to current font height, Android version has no Analyze yet - put info there.

--- src/libs/misc/help/tmainhelp.cpp
+++ src/libs/misc/help/tmainhelp.cpp
@@ -25,34 +25,44 @@
 #include <QtWidgets/qapplication.h>
 #include <QtWidgets/qscroller.h>
 
-#define PIXSIZE (28)
 
 
 /*static*/
+#if defined (Q_OS_ANDROID)
+  inline int getPixSize() { return qApp->fontMetrics().height() * 1.2; }
+#else
+  inline int getPixSize() { return qApp->fontMetrics().height() * 1.5; }
+#endif
+
+
 QString TmainHelp::exerciseAndExamText() {
 	QString helpTxt;
+  QString br = QStringLiteral("<br>");
 	helpTxt = youWillLearnText() + " ";
 	helpTxt += QApplication::translate("TmainHelp", "Much depends on the chosen level. Press %1 button to create your own questions set (a level).").
-			arg(" &nbsp; " + pixToHtml(Tpath::img("levelCreator"), PIXSIZE) + " &nbsp; ");
-  helpTxt += "<br>" + QApplication::translate("TmainHelp", "Select a level and warm up yourself exercising. Then try to pass an exam.");
-	helpTxt += "<br>" + duringExercisingText();
-	helpTxt += "<br>" + duringExamsText();
+			arg(QLatin1String(" &nbsp; ") + pixToHtml(Tpath::img("levelCreator"), getPixSize()) + QLatin1String(" &nbsp; "));
+  helpTxt += br + QApplication::translate("TmainHelp", "Select a level and warm up yourself exercising. Then try to pass an exam.");
+	helpTxt += br + duringExercisingText();
+	helpTxt += br + duringExamsText();
 	return helpTxt;
 }
 
 
 QString TmainHelp::youWillLearnText() {
-	return QApplication::translate("TmainHelp", "You will learn by answering questions. To answer, you can play, sing, put in the name of a note, and so on.");
+	return QApplication::translate("TmainHelp",
+                                 "You will learn by answering questions. To answer, you can play, sing, put in the name of a note, and so on.");
 }
 
 
 QString TmainHelp::duringExercisingText() {
-	return QApplication::translate("TmainHelp", "During exercising %1 the program will be your understanding and friendly teacher - it will show you corrected answers if you miss.").arg(" &nbsp; " + pixToHtml(Tpath::img("practice"), 32) + " &nbsp; ");
+  QString nbsp3 = QLatin1String(" &nbsp; ");
+	return QApplication::translate("TmainHelp", "During exercising %1 the program will be your understanding and friendly teacher - it will show you corrected answers if you miss.").arg(nbsp3 + pixToHtml(Tpath::img("practice"), getPixSize() * 1.3) + nbsp3);
 }
 
 
 QString TmainHelp::duringExamsText() {
-	return QApplication::translate("TmainHelp", "During exams %1 Nootka will be your strict and &quot;old school&quot; master. Any mistake will be penalized with additional questions...<br>When you pass an exam you got a certificate!").arg(" &nbsp; " + pixToHtml(Tpath::img("exam"), 32) + " &nbsp; ");
+  QString nbsp3 = QLatin1String(" &nbsp; ");
+	return QApplication::translate("TmainHelp", "During exams %1 Nootka will be your strict and &quot;old school&quot; master. Any mistake will be penalized with additional questions...<br>When you pass an exam you got a certificate!").arg(nbsp3 + pixToHtml(Tpath::img("exam"), getPixSize() * 1.3) + nbsp3);
 }
 
 
@@ -61,30 +71,39 @@ QString TmainHelp::duringExamsText() {
 
 
 TmainHelp::TmainHelp(QWidget* parent) :
-    QWidget(parent)
-{ 
-  QVBoxLayout *lay = new QVBoxLayout;
-  QString helpTxt = "<br><b>" + QApplication::translate("TmainHelp", "Using Nootka may be divided into three stages:", "Don't try to translate the entries in this context/section too strict, rather use some nice words into your language to describe it. The statements like '%1' are images(icons) inside the text");
-  helpTxt += "</b><table><tr><td>&nbsp;</td><td><hr><b>";
-  
-  helpTxt += QApplication::translate("TmainHelp", "I. Discovering") + "</b><br>";
+  QWidget(parent)
+{
+  int pixSize = getPixSize();
+  QString bbrEnd = QLatin1String("</b><br>");
+  QString nbsp3 = QLatin1String(" &nbsp; ");
+  QString helpTxt = QLatin1String("<br><b>") + QApplication::translate("TmainHelp", "Using Nootka may be divided into three stages:", "Don't try to translate the entries in this context/section too strict, rather use some nice words into your language to describe it. The statements like '%1' are images(icons) inside the text");
+  helpTxt += QLatin1String("</b><table><tr><td>&nbsp;</td><td><hr><b>");
+
+  helpTxt += QApplication::translate("TmainHelp", "I. Discovering") + bbrEnd;
   helpTxt += QApplication::translate("TmainHelp", "Exploring the interface of Nootka and how musical scores work. Just click on elements of the interface to see and get to know Nootka. Also, you can play or sing if you have a mic or web-cam.") + "<br>";
   helpTxt += QApplication::translate("TmainHelp", "Press %1 buttons to see help and %2 button to adjust Nootka to your preference.").
-      arg(" &nbsp; " + pixToHtml(Tpath::img("about"), PIXSIZE) + " <span style=\"font-size: x-large;\"> + </span> " + pixToHtml(Tpath::img("help"), PIXSIZE)  + " &nbsp; ").
-      arg(" &nbsp; " + pixToHtml(Tpath::img("systemsettings"), PIXSIZE) + " &nbsp; ");
+      arg(nbsp3 + pixToHtml(Tpath::img("logo"), pixSize * 1.8) + QLatin1String(" <span style=\"font-size: x-large;\"> + </span> ")
+                            + pixToHtml(Tpath::img("help"), pixSize)  + nbsp3).
+      arg(nbsp3 + pixToHtml(Tpath::img("systemsettings"), pixSize) + nbsp3);
   helpTxt += ThelpDialogBase::onlineDocP("getting-started");
-  
-  helpTxt += "<hr><b>" + QApplication::translate("TmainHelp", "II. Exercises and exams") + "</b><br>";
+
+  helpTxt += "<hr><b>" + QApplication::translate("TmainHelp", "II. Exercises and exams") + bbrEnd;
 	helpTxt += exerciseAndExamText();
   helpTxt += ThelpDialogBase::onlineDocP("exercises");
-	
-  helpTxt += "<hr><b>" + QApplication::translate("TmainHelp", "III. Analyzing") + "</b><br>";
+
+  helpTxt += QLatin1String("<hr><b>") + QApplication::translate("TmainHelp", "III. Analyzing") + bbrEnd;
+#if defined (Q_OS_ANDROID)
+  helpTxt += QLatin1String("This feature has been not ready yet in Android version.<br><b>You may transfer Nootka files to desktop computer and see results in Nootka version there.");
+#else
   helpTxt += QApplication::translate("TmainHelp", "Nootka will tell you about what you've been thinking for so long... and about the progress you've been making so far....<br>Press %1 button to see and to analyze the results of your exams, find your weak points, and improve.").
-    arg(" &nbsp; " + pixToHtml(Tpath::img("charts"), PIXSIZE) + " &nbsp; ");
+    arg(nbsp3 + pixToHtml(Tpath::img("charts"), pixSize) + nbsp3);
+#endif
   helpTxt += ThelpDialogBase::onlineDocP("analyze");
-  helpTxt += "</td></tr></table>";
-  
-  helpTxt += "<hr><b><span style=\"font-size: xx-large;\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + QApplication::translate("TmainHelp", "Have fun!") + "</span></b>";
+  helpTxt += QLatin1String("</td></tr></table>");
+
+  helpTxt += QLatin1String("<hr><b><span style=\"font-size: xx-large;\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;")
+          + QApplication::translate("TmainHelp", "Have fun!") + QLatin1String("</span></b>");
+
   QTextBrowser *helpEdit = new QTextBrowser(this);
   helpEdit->setHtml(helpTxt);
 	helpEdit->setReadOnly(true);
@@ -96,6 +115,8 @@ TmainHelp::TmainHelp(QWidget* parent) :
   helpEdit->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
 #endif
 // 	qDebug() << helpEdit->toHtml();
+
+  auto lay = new QVBoxLayout;
   lay->addWidget(helpEdit);
   setLayout(lay);
 }
