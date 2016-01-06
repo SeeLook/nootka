140308584da4864ecab02656247e625de2bfdfd9
SeeLook <seelook@gmail.com>
2016-01-06 17:42:30 +0100

    Adjusted MyTransforms::init() call for cleaned Tartini code. Minimal note duration set to 150 ms by default.

--- src/libs/sound/tpitchfinder.cpp
+++ src/libs/sound/tpitchfinder.cpp
@@ -76,7 +76,7 @@ TpitchFinder::TpitchFinder(QObject* parent) :
 	setSampleRate(m_aGl->rate);
 	m_channel = new Channel(this, aGl()->windowSize);
   m_transforms = new MyTransforms();
-	m_transforms->init(m_aGl, aGl()->windowSize, 0, aGl()->rate, aGl()->equalLoudness);
+  m_transforms->init(m_aGl, aGl()->windowSize, 0, aGl()->rate);
 	moveToThread(m_thread);
 	connect(m_thread, &QThread::started, this, &TpitchFinder::startPitchDetection);
 	connect(m_thread, &QThread::finished, this, &TpitchFinder::processed);
@@ -170,7 +170,7 @@ void TpitchFinder::resetFinder() {
       m_averVolume = 0.0;
       m_transforms->uninit();
       m_channel = new Channel(this, aGl()->windowSize);
-      m_transforms->init(aGl(), aGl()->windowSize, 0, aGl()->rate, aGl()->equalLoudness);
+      m_transforms->init(m_aGl, aGl()->windowSize, 0, aGl()->rate);
 //       qDebug() << "reset channel";
   }
 //   qDebug() << "framesPerChunk" << m_aGl->framesPerChunk << "windowSize" << m_aGl->windowSize
