39de0afb69d2ba293f90be7b090396dfc474fb0b
SeeLook <SeeLook@localhost>
2016-01-27 10:33:16 +0100

    Updated scripts to prepare Mac Os bundle:
    added 'deploy' target to convert libraries paths in bundle
    install.sh just copies 3d party libs to install bundle
    'runinplace' target adjusted to new build
    generating version nr to plist adds 1000 to keep number bigger than Nootka 1.0 had
    using otool, install_name_tool is no more necessary
    (grafted from bf61ccda07cc97f8999ebc1c50a1cd04914a76d4)

--- packaging/make-noinstall.sh
+++ packaging/make-noinstall.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 #################################################################################
-# This script creates links necessary to run Nootka without installation
+# This script creates links necessary to run Nootka without installation (in directory where it is built)
 # Params are:
 # SRC_DIR - source dir
-# DST_DIR - destination dir
+# DST_DIR - destination dir (Linux: nootka/../share/nootka | Mac nootka/../Resources)
 #################################################################################
 
 SRC_DIR=$1
@@ -13,22 +13,27 @@ printf "\033[01;35mLinking source files necessary to run Nootka"
 printf "\033[01;00m"
 echo
 
-echo $DST_DIR
+#echo $DST_DIR
 
-if [ $( echo $DST_DIR| grep 'nootka.app')  ]; then
-	echo "Nac os"
+if [ $( echo $DST_DIR| grep 'Resources')  ]; then
+  echo "Linking for Mac Os"
+  DST_DIR=$2/
 else
-# 	echo "Linux"
-	if [ -d $DST_DIR/share ]; then
-		echo "Already done"
-		exit
-	else
-		mkdir -p $DST_DIR/share/nootka
-		ln -s $SRC_DIR/fonts $DST_DIR/share/nootka/
-		ln -s $SRC_DIR/changes $DST_DIR/share/nootka/
-		ln -s $SRC_DIR/gpl $DST_DIR/share/nootka/
-		ln -s $SRC_DIR/picts $DST_DIR/share/nootka/
-		ln -s $SRC_DIR/lang $DST_DIR/share/nootka/
-		ln -s $SRC_DIR/sounds $DST_DIR/share/nootka/
-	fi
+  echo "Linking for Linux"
+  DST_DIR=$2/share/nootka/
 fi
+
+echo $DST_DIR
+if [ -d $DST_DIR ]; then
+  echo "Already done... Exiting!"
+  exit
+else
+  mkdir -p $DST_DIR
+  ln -s $SRC_DIR/fonts $DST_DIR
+  ln -s $SRC_DIR/changes $DST_DIR
+  ln -s $SRC_DIR/gpl $DST_DIR
+  ln -s $SRC_DIR/picts $DST_DIR
+  ln -s $SRC_DIR/lang $DST_DIR
+  ln -s $SRC_DIR/sounds $DST_DIR
+fi
+
