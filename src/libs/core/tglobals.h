5a30ef93f4d24c00bb97fbe1c6b51c0452101591
SeeLook <seelook@gmail.com>
2016-02-16 15:31:22 +0100

    Cleaned Tglobals code

--- src/libs/core/tglobals.h
+++ src/libs/core/tglobals.h
@@ -21,10 +21,11 @@
 #define TGLOBALS_H
 
 #include <nootkacoreglobal.h>
-#include <QString>
-#include <QColor>
+#include <QtCore/qstring.h>
+#include <QtGui/qcolor.h>
 #include <music/tnote.h>
 #include <music/tinstrument.h>
+
 
 class TlayoutParams;
 class TscoreParams;
@@ -39,22 +40,22 @@ class NOOTKACORE_EXPORT Tglobals
 public:
 
       /** If @p true, setting are loaded from temporary config file */
-  Tglobals(bool fromTemp = false);
+  Tglobals();
   ~Tglobals();
 
       /** This method return application install path - path from where Nootka was started. */
   static QString getInstPath(QString appInstPath);
-  static QString& path; /** Reference to Tpath::main - Nootka resources path */
+  static QString& path; /**< Reference to Tpath::main - Nootka resources path */
 
-  void storeSettings(QSettings* cfg); /** Saves settings stored in @p cfg */
-  void loadSettings(QSettings* cfg); /** Loads Nootka settings from file to @p cfg */
+  void storeSettings(QSettings* cfg); /**< Saves settings stored in @p cfg */
+  void loadSettings(QSettings* cfg); /**< Loads Nootka settings from file to @p cfg */
 
   QString version;
-  bool isFirstRun; /** to show or skip first run wizard*/
-  QString lang; /** Application language. If empty - selected from system info*/
-  QSettings *config; /** Pointer to QSettings instance of Nootka */
-  bool useAnimations; /** to show GUI animations. */
-  bool enableTouch; /** When true, touch events are propagated */
+  bool isFirstRun; /**< to show or skip first run wizard*/
+  QString lang; /**< Application language. If empty - selected from system info*/
+  QSettings *config; /**< Pointer to QSettings instance of Nootka */
+  bool useAnimations; /**< to show GUI animations. */
+  bool enableTouch; /**< When true, touch events are propagated */
 
         /** Due to bug in version 0.8.95 it keeps value of user declared instrument to fix incorrect levels
          * or -1 to display a dialog for user to declare it.  */
@@ -67,18 +68,18 @@ public:
         * For sound and exam there are pointers to appropirate classes with theirs parameters. */
 
   //============ score settings =============================================================
-  TscoreParams *S; /** Score parameters */
+  TscoreParams *S; /**< Score parameters */
 
 
     /** Guessing solfege name style from current locale setting. F.e.: ru is e_russian_Ci */
   Tnote::EnameStyle getSolfegeStyle();
 
   //============ guitar settings =============================================================
-  Einstrument instrument; /** Type of instrument - classical guitar default */
-  unsigned char GfretsNumber; /** default 19 */
-  bool GisRightHanded; /** default true */
-  bool GshowOtherPos; /** Shows other possibilities of note (sound) on the fretboard (default true) */
-  QColor GfingerColor; /** rules the same like in S->enharmNotesColor */
+  Einstrument instrument; /**< Type of instrument - classical guitar default */
+  unsigned char GfretsNumber; /**< default 19 */
+  bool GisRightHanded; /**< default true */
+  bool GshowOtherPos; /**< Shows other possibilities of note (sound) on the fretboard (default true) */
+  QColor GfingerColor; /**< rules the same like in S->enharmNotesColor */
   QColor GselectedColor;
 
       /** Actual tune of the guitar also with information about strings number
@@ -89,8 +90,8 @@ public:
       /** It returns real string number (0 - 5) when @param strNr
        * is sorted number from highest (0) to lowest (5) */
   qint8 strOrder(qint8 strNr) { return m_order[strNr]; }
-  Tnote hiString(); /** Returns the highest (usually first - 0) string. */
-  Tnote loString(); /** Returns the lowest (usually last) string */
+  Tnote hiString(); /**< Returns the highest (usually first - 0) string. */
+  Tnote loString(); /**< Returns the lowest (usually last) string */
 
       /** The highest available note in current tune with current fret number */
   Tnote hiNote() { return Tnote(hiString().chromatic() + GfretsNumber); }
@@ -102,17 +103,17 @@ public:
   QList<QVariant> GmarkedFrets;
 
 
-  TexamParams *E; /** Exam parameters */
+  TexamParams *E; /**< Exam parameters */
   QColor EquestionColor; // red
   QColor EanswerColor; // green
   QColor EnotBadColor; // #ff8000 (orange)
 
   TaudioParams *A;  /** Audio parameters */
-  TlayoutParams *L; /** Main window Layout params. */
+  TlayoutParams *L; /**< Main window Layout params. */
 
 private:
-  Ttune *m_tune; /** current guitar tune */
-  qint8 m_order[6]; /** Strings' order is determined in @param setTune() method */
+  Ttune *m_tune; /**< current guitar tune */
+  qint8 m_order[6]; /**< Strings order is determined in @param setTune() method */
 
 };
 #endif // TGLOBALS_H
