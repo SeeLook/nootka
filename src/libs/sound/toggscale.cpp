fd8190d85ba61f906f38264230bab7a5a6ef35e7
SeeLook <seelook@gmail.com>
2016-04-01 11:14:20 +0200

    Fixed glitches of playing sound: emitting signal when decoded ogg data is ready and starting playing then instead of blocking main thread with sleep and waiting for decoding. Code cleaned.

--- src/libs/sound/toggscale.cpp
+++ src/libs/sound/toggscale.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2013-2015 by Tomasz Bojczuk                             *
+ *   Copyright (C) 2013-2016 by Tomasz Bojczuk                             *
  *   seelook@gmail.com                                                     *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
@@ -20,11 +20,11 @@
 #include "toggscale.h"
 #include <music/tinstrument.h>
 #include <tpath.h>
-#include <QFile>
-#include <QDataStream>
-#include <QDebug>
-#include <QThread>
-#include <QFileInfo>
+#include <QtCore/qfile.h>
+#include <QtCore/qdatastream.h>
+#include <QtCore/qdebug.h>
+#include <QtCore/qthread.h>
+#include <QtCore/qfileinfo.h>
 #include <stdlib.h>
 #include <unistd.h>
 
@@ -98,12 +98,12 @@ ToggScale::ToggScale() :
   m_oggConnected(false), m_touchConnected(false),
   m_instrument(-1)
 {
-	m_touch = new soundtouch::SoundTouch();
-	m_touch->setChannels(1);
+  m_touch = new soundtouch::SoundTouch();
+  m_touch->setChannels(1);
 #if defined (Q_OS_UNIX) // increase minimal audio data must to be processed when system works with PulseAudio
-	QFileInfo pulseBin("/usr/bin/pulseaudio");
-	if (pulseBin.exists()) // it is necessary both for Nootka with native PA and PA in ALSA bridge mode
-		minDataAmount = 15000;
+  QFileInfo pulseBin("/usr/bin/pulseaudio");
+  if (pulseBin.exists()) // it is necessary both for Nootka with native PA and PA in ALSA bridge mode
+    minDataAmount = 15000;
 #endif
   moveToThread(m_thread);
   connect(m_thread, SIGNAL(started()), this, SLOT(decodeOgg()));
@@ -121,7 +121,7 @@ void ToggScale::deleteData() {
   stopDecoding();
   if (m_thread->isRunning()) {
     m_thread->terminate();
-    m_thread->quit();  
+    m_thread->quit();
   }
   if (m_oggInMemory) {
     delete m_oggInMemory;
@@ -135,33 +135,36 @@ void ToggScale::deleteData() {
 
 
 qint16 ToggScale::getSample(int offset) {
-    return m_pcmBuffer[offset];
+  return m_pcmBuffer[offset];
 }
 
 
 
 void ToggScale::setNote(int noteNr) {
-  if (noteNr == m_prevNote)
+  if (noteNr == m_prevNote) {
+    emit oggReady();
     return;
-	int baseNote = noteNr;
+  }
+
+  int baseNote = noteNr;
   m_isReady = false;
-	if (noteNr < m_firstNote || noteNr > m_lastNote) { // prepare SoundTouch
-		if (noteNr < m_firstNote) {
-				baseNote = m_firstNote;
-				m_innerOffset = noteNr - m_firstNote;
-		} else if (noteNr > m_lastNote) {
-				baseNote = m_lastNote;
-				m_innerOffset = noteNr - m_lastNote;
-		}		
-	} else
-			m_innerOffset = 0.0;
-	adjustSoundTouch();
+  if (noteNr < m_firstNote || noteNr > m_lastNote) { // prepare SoundTouch
+    if (noteNr < m_firstNote) {
+        baseNote = m_firstNote;
+        m_innerOffset = noteNr - m_firstNote;
+    } else if (noteNr > m_lastNote) {
+        baseNote = m_lastNote;
+        m_innerOffset = noteNr - m_lastNote;
+    }
+  } else
+      m_innerOffset = 0.0;
+  adjustSoundTouch();
   int fasterOffset = 1000;
-	if (baseNote - m_firstNote == 0)
-			fasterOffset = 0;
+  if (baseNote - m_firstNote == 0)
+      fasterOffset = 0;
   stopDecoding();
   m_prevNote = noteNr;
-	int ret = ov_pcm_seek(&m_ogg, (baseNote - m_firstNote) * 44100 * 2 - fasterOffset);
+  int ret = ov_pcm_seek(&m_ogg, (baseNote - m_firstNote) * 44100 * 2 - fasterOffset);
   m_thread->start();
 }
 
@@ -183,60 +186,60 @@ void ToggScale::setPitchOffset(float pitchOff) {
 
 
 bool ToggScale::loadAudioData(int instrument) {
-	QString fileName;
-	if (instrument != m_instrument) {
-		switch ((Einstrument)instrument) {
-			case e_classicalGuitar:
-				fileName = Tpath::sound("classical-guitar");
-				m_firstNote = -11; m_lastNote = 41;
-				break;
-			case e_electricGuitar:
-				fileName = Tpath::sound("electric-guitar");
-				m_firstNote = -11; m_lastNote = 41;
-				break;
-			case e_bassGuitar:
-				fileName = Tpath::sound("bass-guitar");
-				m_firstNote = -24; m_lastNote = 21;
-				break;
-			default:
-				return false;
-		}
-	} else
-			return false;
+  QString fileName;
+  if (instrument != m_instrument) {
+    switch ((Einstrument)instrument) {
+      case e_classicalGuitar:
+        fileName = Tpath::sound("classical-guitar");
+        m_firstNote = -11; m_lastNote = 41;
+        break;
+      case e_electricGuitar:
+        fileName = Tpath::sound("electric-guitar");
+        m_firstNote = -11; m_lastNote = 41;
+        break;
+      case e_bassGuitar:
+        fileName = Tpath::sound("bass-guitar");
+        m_firstNote = -24; m_lastNote = 21;
+        break;
+      default:
+        return false;
+    }
+  } else
+      return false;
 
   QFile oggFile(fileName);
   if (!oggFile.exists())
       return false;
-  
+
   oggFile.open(QIODevice::ReadOnly);
   QDataStream oggStream(&oggFile);
-	if (m_oggInMemory)
-			delete m_oggInMemory;
+  if (m_oggInMemory)
+      delete m_oggInMemory;
   m_oggInMemory = new qint8[oggFile.size()];
   oggStream.readRawData((char*)m_oggInMemory, oggFile.size());
-  
+
   ov_callbacks myCallBacks;
   m_oggWrap.curPtr = m_oggInMemory;
   m_oggWrap.filePtr = m_oggInMemory;
   m_oggWrap.fileSize = oggFile.size();
-  
+
   oggFile.close();
-	
-	if (m_pcmBuffer)
-			delete m_pcmBuffer;
+
+  if (m_pcmBuffer)
+      delete m_pcmBuffer;
   m_pcmBuffer = new qint16[2 * m_sampleRate];
   myCallBacks.read_func = readOggStatic;
   myCallBacks.seek_func = seekOggStatic;
   myCallBacks.close_func = closeOggStatic;
   myCallBacks.tell_func = tellOggStatic;
-  
+
   int ret = ov_open_callbacks((void*)&m_oggWrap, &m_ogg, NULL, 0, myCallBacks);
-  
+
   if (ret < 0) {
     qDebug() << "cant open ogg stream";
     return false;
   }
-  
+
 //   vorbis_info *oggInfo = ov_info(&m_ogg, -1);
 //   qDebug() << oggInfo->rate << oggInfo->channels << (bool)ov_seekable(&m_ogg);
 //   char **ptr=ov_comment(&m_ogg, -1)->user_comments;
@@ -244,9 +247,9 @@ bool ToggScale::loadAudioData(int instrument) {
 //     while(*ptr){
 //       fprintf(stderr,"%s\n",*ptr);
 //       ++ptr;
-//     }  
-  
-  return true;  
+//     }
+
+  return true;
 }
 
 
@@ -276,8 +279,10 @@ void ToggScale::decodeOgg() {
   while (m_doDecode && loops < 500 && pos < maxSize) {
     read = ov_read(&m_ogg, (char*)m_pcmBuffer + pos, maxSize - pos, 0, 2, 1, &bitStream);
     pos += read;
-    if (pos > minDataAmount) // amount of data needed by single loop of rtAudio outCallBack
+    if (pos > minDataAmount && !m_isReady) { // amount of data needed by single loop of rtAudio outCallBack
       m_isReady = true;
+      emit oggReady();
+    }
     loops++;
   }
   m_isDecoding = false;
@@ -287,17 +292,16 @@ void ToggScale::decodeOgg() {
 
 
 void ToggScale::decodeAndResample() {
-  int bitStream;
   m_isDecoding = true;
   int maxSize = 44100 * 2 - 8192; // two sec. of audio minus some silence on the end
   long int tmpPos = 0, tmpRead = 0;
   uint pos = 0, read = 0;
   int samplesReady = 0;
-  
+
   float **oggChannels;
   float *left ;
   float *tmpTouch = new float[8192];
-  
+
   while (m_doDecode && pos < maxSize) {
     /// 1. Grab audio data from ogg
     if (tmpPos < 172000) { // almost 2 sec. of a note
@@ -310,13 +314,15 @@ void ToggScale::decodeAndResample() {
     }
     samplesReady = m_touch->numSamples();
     if (samplesReady > 0) { /// 3. Get resampled/offsetted data from SoundTouch
-      read = m_touch->receiveSamples((SAMPLETYPE*)tmpTouch, samplesReady);      
+      read = m_touch->receiveSamples((SAMPLETYPE*)tmpTouch, samplesReady);
       for (int i = 0; i < read; i++) /// 4. Convert samples to 16bit integer
           *(m_pcmBuffer + pos + i) = qint16(*(tmpTouch + i) * 32768);
       pos += read;
     }
-    if (pos > minDataAmount) // below this value SoundTouch is not able to prepare data
+    if (pos > minDataAmount && !m_isReady) { // below this value SoundTouch is not able to prepare data
       m_isReady = true;
+      emit oggReady();
+    }
   }
   m_isDecoding = false;
 //   qDebug() << "decodeAndResample finished" << pos;
@@ -342,15 +348,15 @@ void ToggScale::adjustSoundTouch() {
         connect(m_thread, SIGNAL(started()), this, SLOT(decodeAndResample()));
     m_touchConnected = true;
     if (m_oggConnected)
-				disconnect(m_thread, SIGNAL(started()), this, SLOT(decodeOgg()));
+        disconnect(m_thread, SIGNAL(started()), this, SLOT(decodeOgg()));
     m_oggConnected = false;
   } else {
-			if (!m_oggConnected)
-					connect(m_thread, SIGNAL(started()), this, SLOT(decodeOgg()));
-			m_oggConnected = true;
-			if (m_touchConnected)
-					disconnect(m_thread, SIGNAL(started()), this, SLOT(decodeAndResample()));
-			m_touchConnected = false;
+      if (!m_oggConnected)
+          connect(m_thread, SIGNAL(started()), this, SLOT(decodeOgg()));
+      m_oggConnected = true;
+      if (m_touchConnected)
+          disconnect(m_thread, SIGNAL(started()), this, SLOT(decodeAndResample()));
+      m_touchConnected = false;
   }
 }
 
--- src/libs/sound/toggscale.h
+++ src/libs/sound/toggscale.h
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2013-2015 by Tomasz Bojczuk                             *
+ *   Copyright (C) 2013-2016 by Tomasz Bojczuk                             *
  *   seelook@gmail.com                                                     *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
@@ -37,99 +37,102 @@
 using namespace soundtouch;
 
 
-/** 
+/**
 * @class ToggScale manages audio data (musical scale) taken from ogg file.
 * It keeps it in m_oggInMemory array and decode it when setNote is called.
-* Decompressed data are available through getSample() method. 
-* Data is decompressed in separate thread and some SLEEP calls are performed 
+* Decompressed data are available through getSample() method.
+* Data is decompressed in separate thread and some SLEEP calls are performed
 * if data is not ready.
-* 
-* @p setSampleRate() and @p setPitchOffset() control appropirate parameters of note.
-* 
+*
+* @p setSampleRate() and @p setPitchOffset() control appropriate parameters of note.
+*
 * To get sample @p setNote has to be called first.
 * It starts decoding thread which prepares first portion of data.
 * Data is ready only when @p isReady() returns true.
-* Preparing process takes around 1-10 ms (depends on CPU) 
+* Preparing process takes around 1-10 ms (depends on CPU)
 */
 class NOOTKASOUND_EXPORT ToggScale : public QObject
 {
   Q_OBJECT
-  
+
 public:
-  
-    ToggScale(); 
-    virtual ~ToggScale();
-    
-        /** Loads ogg file with scale of given instrument to RAM.
-				 * If everything is OK, returns true.
-				 * Also it is setting low and hi notes of a scale.
-				 * Notes beyond scale are generated with SoundTouch. */
-    bool loadAudioData(int instrument);
-        /** Unloads audio data from buffer. */
-    void deleteData();
-    
-    
-        /** To read ogg data from RAM */
-    struct SoggFile {
-        qint8* curPtr;
-        qint8* filePtr;
-        size_t fileSize;
-    };
-    
-        /** Prepares m_pcmBuffer:
-				 * - determines is pitch offset necessary
-         * - seek ogg 
-         * - starts decoding (re-sampling). 
-         * - stops previous decoding if performed. */
-    void setNote(int noteNr);
-    qint16 getSample(int offset);
-    unsigned int sampleRate() { return m_sampleRate; }
-    
-        /** TRUE when appropirate data amount in a buffer is ready. */
-    bool isReady() { return m_isReady; }
-    
-    void setSampleRate(unsigned int rate);
-		
-        /** Sets decimal offset of a pitch -0.99 to +0.99 */
-    void setPitchOffset(float pitchOff);
-    
-    
+
+  ToggScale();
+  virtual ~ToggScale();
+
+      /** Loads ogg file with scale of given instrument to RAM.
+        * If everything is OK, returns true.
+        * Also it is setting low and hi notes of a scale.
+        * Notes beyond scale are generated with SoundTouch. */
+  bool loadAudioData(int instrument);
+      /** Unloads audio data from buffer. */
+  void deleteData();
+
+
+      /** To read ogg data from RAM */
+  struct SoggFile {
+      qint8* curPtr;
+      qint8* filePtr;
+      size_t fileSize;
+  };
+
+      /** Prepares m_pcmBuffer:
+        * - determines is pitch offset necessary
+        * - seek ogg
+        * - starts decoding (re-sampling).
+        * - stops previous decoding if performed. */
+  void setNote(int noteNr);
+  qint16 getSample(int offset);
+  unsigned int sampleRate() { return m_sampleRate; }
+
+      /** TRUE when appropriate data amount in a buffer is ready. */
+  bool isReady() { return m_isReady; }
+
+  void setSampleRate(unsigned int rate);
+
+      /** Sets decimal offset of a pitch -0.99 to +0.99 */
+  void setPitchOffset(float pitchOff);
+
+signals:
+  void oggReady(); /**< Emitted when appropriate amount of decoded data is ready */
+
+
 protected slots:
-        /** Preforms decoding. Usually is invoked by m_thread.start() 
-         * called from setNote() method. */
-    void decodeOgg();
-        /** Decoding and re-sampling or/and pitch shifting */
-    void decodeAndResample();
-        /** Checks is decoding performed and stops it
-         * by setting m_doDecode=false and waiting for stop. */
-    void stopDecoding();
-    
+      /** Preforms decoding. Usually is invoked by m_thread.start()
+        * called from setNote() method. */
+  void decodeOgg();
+      /** Decoding and re-sampling or/and pitch shifting */
+  void decodeAndResample();
+      /** Checks is decoding performed and stops it
+        * by setting m_doDecode=false and waiting for stop. */
+  void stopDecoding();
+
 private:
-      /** Methods needed by vorbisfile library. */
-    static size_t readOggStatic(void* dst, size_t size1, size_t size2, void* fh);
-    static int    seekOggStatic(void *fh, ogg_int64_t to, int type );
-    static int    closeOggStatic(void* fh);
-    static long   tellOggStatic(void *fh );
-    
-    void adjustSoundTouch(); // sets SoundTouch parameters
-    
+    /** Methods needed by vorbisfile library. */
+  static size_t readOggStatic(void* dst, size_t size1, size_t size2, void* fh);
+  static int    seekOggStatic(void *fh, ogg_int64_t to, int type );
+  static int    closeOggStatic(void* fh);
+  static long   tellOggStatic(void *fh );
+
+  void adjustSoundTouch(); // sets SoundTouch parameters
+
 private:
-    qint8             *m_oggInMemory;
-    OggVorbis_File    m_ogg; /** ogg vorbis handler */
-    qint16            *m_pcmBuffer; /** buffer with decompressed data of selected note. */
-    SoggFile          m_oggWrap; /** Structure wrapped m_oggInMemory used by ogg vorbis. */
-    QThread           *m_thread;
-    unsigned int      m_sampleRate;
-    int               m_prevNote;
-    bool              m_doDecode; /** If new note is going to be decoded it goes to FALSE - then stops decoding loop */
-    bool              m_isDecoding; /** TRUE during decoding/resampling process. */
-    bool              m_isReady;
-    SoundTouch        *m_touch;
-    float             m_pitchOffset; /** Offset set from outside (by user) */
-    float							m_innerOffset; /** Offset calculated in setNote when SoundTouch has to generate note. */
-    bool              m_oggConnected, m_touchConnected;
-		int								m_firstNote, m_lastNote; /** Numbers of first and last notes in file with scale. */
-		int								m_instrument; /** current instrument which samples are loaded */
+  qint8             *m_oggInMemory;
+  OggVorbis_File     m_ogg; /**< ogg vorbis handler */
+  qint16            *m_pcmBuffer; /**< buffer with decompressed data of selected note. */
+  SoggFile           m_oggWrap; /**< Structure wrapped m_oggInMemory used by ogg vorbis. */
+  QThread           *m_thread;
+  unsigned int       m_sampleRate;
+  int                m_prevNote;
+  bool               m_doDecode; /**< If new note is going to be decoded it goes to FALSE - then stops decoding loop */
+  bool               m_isDecoding; /**< TRUE during decoding/resampling process. */
+  bool               m_isReady;
+  SoundTouch        *m_touch;
+  float              m_pitchOffset; /**< Offset set from outside (by user) */
+  float              m_innerOffset; /**< Offset calculated in setNote when SoundTouch has to generate note. */
+  bool               m_oggConnected, m_touchConnected;
+  int                m_firstNote, m_lastNote; /**< Numbers of first and last notes in file with scale. */
+  int                m_instrument; /**< current instrument which samples are loaded */
 
 };
 
