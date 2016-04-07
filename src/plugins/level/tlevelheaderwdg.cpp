a1b4bad50e003647a8a63e00d4b8a2ae73dc7ae7
SeeLook <945374+SeeLook@users.noreply.github.com>
2016-04-07 13:23:28 +0200

    Android: Improved naming level in creator dialog (header dialog size and background)

--- src/plugins/level/tlevelheaderwdg.cpp
+++ src/plugins/level/tlevelheaderwdg.cpp
@@ -1,6 +1,6 @@
 /***************************************************************************
- *   Copyright (C) 2011-2014 by Tomasz Bojczuk  				                   *
- *   seelook@gmail.com   						                                       *
+ *   Copyright (C) 2011-2016 by Tomasz Bojczuk                             *
+ *   seelook@gmail.com                                                     *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -12,50 +12,62 @@
  *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
  *   GNU General Public License for more details.                          *
  *                                                                         *
- *  You should have received a copy of the GNU General Public License	     *
+ *  You should have received a copy of the GNU General Public License      *
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
  ***************************************************************************/
 
 
 #include "tlevelheaderwdg.h"
+#include <qtr.h>
+#if defined (Q_OS_ANDROID)
+  #include <tmtr.h>
+#endif
 #include <QtWidgets/QtWidgets>
 
 
 TlevelHeaderWdg::TlevelHeaderWdg(QWidget *parent) :
-    QDialog(parent)
+  QDialog(parent)
 {
-    setWindowFlags(Qt::CustomizeWindowHint | Qt::Dialog);
-    QVBoxLayout *mainLay = new QVBoxLayout;
-    QLabel *nameLab = new QLabel(tr("Level name:"), this);
-    mainLay->addWidget(nameLab);
-    nameEd = new QLineEdit(this);
-    nameEd->setMaxLength(20);
-    nameEd->setText(tr("new level"));
-    mainLay->addWidget(nameEd);
-    QLabel *descLab = new QLabel(tr("Level description:"), this);
-    mainLay->addWidget(descLab);
-    descEd = new QTextEdit(this);
-    descEd->setFixedHeight(70);
-    descEd->setLineWrapMode(QTextEdit::FixedColumnWidth);
-    descEd->setLineWrapColumnOrWidth(35);
-    mainLay->addWidget(descEd);
-    okBut = new QPushButton(tr("OK"), this);
-    mainLay->addWidget(okBut,1,Qt::AlignCenter);
+  setWindowFlags(Qt::CustomizeWindowHint | Qt::Dialog);
+  auto mainLay = new QVBoxLayout;
+  auto nameLab = new QLabel(tr("Level name:"), this);
+  mainLay->addWidget(nameLab);
+  nameEd = new QLineEdit(this);
+  nameEd->setMaxLength(25);
+  nameEd->setText(tr("new level"));
+  mainLay->addWidget(nameEd);
+  QLabel *descLab = new QLabel(tr("Level description:"), this);
+  mainLay->addWidget(descLab);
+  descEd = new QTextEdit(this);
+  descEd->setFixedHeight(fontMetrics().boundingRect(QStringLiteral("A")).height() * 4);
+  descEd->setFixedWidth(fontMetrics().boundingRect(QStringLiteral("w")).width() * 30);
+  descEd->setLineWrapMode(QTextEdit::FixedColumnWidth);
+  descEd->setLineWrapColumnOrWidth(35);
+  mainLay->addWidget(descEd);
+  okBut = new QPushButton(qTR("QDialogButtonBox", "OK"), this);
+  mainLay->addWidget(okBut, 1, Qt::AlignCenter);
 
-    setLayout(mainLay);
+  setLayout(mainLay);
 
-    connect(descEd, SIGNAL(textChanged()), this, SLOT(onTextChanged()));
-    connect(okBut, SIGNAL(clicked()), this, SLOT(accept()));
+#if defined (Q_OS_ANDROID)
+  move((Tmtr::screenWidth() - descEd->width()) / 2, Tmtr::fingerPixels() / 2);
+  // for better visibility where there is no window frame on mobile change background color
+  setObjectName(QStringLiteral("levelHeader"));
+  setStyleSheet(QStringLiteral("QDialog#levelHeader { background-color: #A1A1A1; }")); // dark gray
+#endif
+
+  connect(descEd, SIGNAL(textChanged()), this, SLOT(onTextChanged()));
+  connect(okBut, SIGNAL(clicked()), this, SLOT(accept()));
 }
 
 QStringList TlevelHeaderWdg::getLevelName() {
-    exec();
-    QStringList list;
-    list << nameEd->text() << descEd->toPlainText();
-    return list;
+  exec();
+  QStringList list;
+  list << nameEd->text() << descEd->toPlainText();
+  return list;
 }
 
 void TlevelHeaderWdg::onTextChanged() {
-    if (descEd->toPlainText().length() > 120 )
-        descEd->setPlainText(descEd->toPlainText().right(120));
+  if (descEd->toPlainText().length() > 120 )
+    descEd->setPlainText(descEd->toPlainText().right(120));
 }
