3176cc1f481597243b4c0dc660d725ba1ffa5028
SeeLook <seelook@gmail.com>
2016-07-06 15:26:28 +0200

    Initialization of TscoreTie class

---
+++ src/libs/score/tscoretie.h
@@ -0,0 +1,47 @@
+/***************************************************************************
+ *   Copyright (C) 2016 by Tomasz Bojczuk                                  *
+ *   seelook@gmail.com                                                     *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
+ ***************************************************************************/
+
+#ifndef TSCORETIE_H
+#define TSCORETIE_H
+
+
+#include <QtCore/qobject.h>
+
+
+class QGraphicsSimpleTextItem;
+class TscoreNote;
+
+
+/**
+ * The bow connecting two (or more) notes (ligature)
+ */
+class TscoreTie : public QObject
+{
+
+  Q_OBJECT
+
+explicit TscoreTie(TscoreNote* sn);
+
+protected:
+  void onNoteChnged(TscoreNote* sn);
+
+private:
+  QGraphicsSimpleTextItem           *m_tie;
+};
+
+#endif // TSCORETIE_H
