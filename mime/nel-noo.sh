df58086bd56b5e6687027b6dfece6a495848738a
SeeLook <seelook@gmail.com>
2016-07-30 22:11:44 +0200

    Global changes (graft-able) for Spanish localization

--- mime/nel-noo.sh
+++ mime/nel-noo.sh
@@ -6,16 +6,17 @@ cat <<EOF
   <mime-type type="application/x-nootka-nel">
     <sub-class-of type="application/data"/>
     <sub-class-of type="application/bin"/>
-    <comment>Nootka exam level</comment>
+    <comment>Nootka level file</comment>
     <comment xml:lang="cs">Úroveň zkoušky programu Nootka</comment>
     <comment xml:lang="de">Nootka-Level Datei</comment>
+    <comment xml:lang="es">Fichero de nivel de Nootka</comment>
     <comment xml:lang="fr">Fichier de niveau Nootka</comment>
     <comment xml:lang="pl">Poziom egzaminu programu Nootka</comment>
     <comment xml:lang="ru">Файл Нутки с уровнем</comment>
     <icon name="nootka-level">$1/share/pixmaps/nootka-level.png</icon>
     <glob pattern="*.nel"/>
     <acronym>NEL</acronym>
-    <expanded-acronym>Nootka Exam's Level</expanded-acronym>
+    <expanded-acronym>Nootka Exam Level</expanded-acronym>
   </mime-type>
 
   <mime-type type="application/x-nootka-noo">
@@ -24,6 +25,7 @@ cat <<EOF
     <comment>Nootka exam file</comment>
     <comment xml:lang="cs">Zkušební soubor programu Nootka</comment>
     <comment xml:lang="de">Nootka-Prüfungs Datei</comment>
+    <comment xml:lang="es">Fichero de examen de Nootka</comment>
 		<comment xml:lang="fr">Fichier d'examen Nootka</comment>
     <comment xml:lang="pl">Plik egzaminu programu Nootka</comment>
     <comment xml:lang="ru">Файл Нутки с экзаменом</comment>
