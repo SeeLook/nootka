8081ae29ef89e61939701a8b46fe28096b7af5b7
SeeLook <945374+SeeLook@users.noreply.github.com>
2016-10-22 19:52:43 +0200

    Ticks width and gaps between depend on screen DPI, otherwise on high DPI screens volume/intonation views have too many ticks too close each. Volume view displays minimal value on the left, get rid of tool tip.

--- src/libs/sound/widgets/tintonationview.cpp
+++ src/libs/sound/widgets/tintonationview.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2013-2015 by Tomasz Bojczuk                             *
+ *   Copyright (C) 2013-2016 by Tomasz Bojczuk                             *
  *   seelook@gmail.com                                                     *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
@@ -19,18 +19,15 @@
 
 #include "tintonationview.h"
 #include "tleavemenu.h"
-#include <QPainter>
-#include <QMouseEvent>
-#include <QVBoxLayout>
-#include <QComboBox>
-#include <QLabel>
-#include <QDebug>
-#include <QTimer>
+#include <QtGui/qpainter.h>
+#include <QtGui/qevent.h>
+#include <QtWidgets/qboxlayout.h>
+#include <QtWidgets/qcombobox.h>
+#include <QtWidgets/qlabel.h>
+#include <QtCore/qdebug.h>
+#include <QtCore/qtimer.h>
 #include <math.h>
 
-
-#define TICK_WIDTH (2)
-#define TICK_GAP (3)
 
 /*static*/
 float TintonationView::getThreshold(TintonationView::Eaccuracy acc) {
@@ -176,12 +173,12 @@ void TintonationView::paintEvent(QPaintEvent* ) {
 					leftThickColor = thickColor; rightThickColor = tc;
 			} else {
 					leftThickColor = tc; rightThickColor = thickColor; }
-			int xx = m_noteX - ((i + 1) * (TICK_GAP + TICK_WIDTH));
+			int xx = m_noteX - ((i + 1) * (tickGap() + tickWidth()));
 			float yy = (float)i * m_hiTickStep + 1.0f;
-			painter.setPen(QPen(leftThickColor, TICK_WIDTH, Qt::SolidLine, Qt::RoundCap));
+			painter.setPen(QPen(leftThickColor, tickWidth(), Qt::SolidLine, Qt::RoundCap));
 			painter.drawLine(QLineF(xx, (height() - yy) / 2, xx, height() - (height() - yy) / 2));
-			painter.setPen(QPen(rightThickColor, TICK_WIDTH, Qt::SolidLine, Qt::RoundCap));
-			xx = (width() - m_noteX) + ((i + 1) * (TICK_GAP + TICK_WIDTH)) - TICK_WIDTH;
+			painter.setPen(QPen(rightThickColor, tickWidth(), Qt::SolidLine, Qt::RoundCap));
+			xx = (width() - m_noteX) + ((i + 1) * (tickGap() + tickWidth())) - tickWidth();
 			painter.drawLine(QLineF(xx, (height() - yy) / 2, xx, height() - (height() - yy) / 2));
   }
 }
@@ -190,7 +187,7 @@ void TintonationView::paintEvent(QPaintEvent* ) {
 void TintonationView::resizeEvent(QResizeEvent* ) {
   resizeIt(height());
   m_noteX = (width() - noteBound.width() * 2) / 2;
-  m_ticksCount = (m_noteX / (TICK_WIDTH + TICK_GAP));
+  m_ticksCount = (m_noteX / (tickWidth() + tickGap()));
   m_hiTickStep = ((float)height() * 0.66) / m_ticksCount;
   m_tickColors.clear();
   for (int i = 0; i < m_ticksCount; i++) {
