d07f20a42bac1664624311455e14664c04c30164
SeeLook <seelook@gmail.com>
2017-01-09 22:15:44 +0100

    Initial commit for scorek library, with music classes copied from core (grafted from ff4df55fc81684badb91b537dc1efb98540d6685)

---
+++ src/libs/scorek/music/tmeter.h
@@ -0,0 +1,66 @@
+/***************************************************************************
+ *   Copyright (C) 2014-2016 by Tomasz Bojczuk                             *
+ *   seelook@gmail.com                                                     *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
+ ***************************************************************************/
+
+#ifndef TMETER_H
+#define TMETER_H
+
+#include <nootkacoreglobal.h>
+#include <QtCore/qxmlstream.h>
+#include <QtGui/qpixmap.h>
+
+
+/**
+ * Describes musical time signature
+ */
+class NOOTKACORE_EXPORT Tmeter
+{
+
+public:
+  enum Emeter {
+    e_none = 0,
+    e_2_4 = 1, e_3_4 = 2, e_4_4 = 4, e_5_4 = 8, e_6_4 = 16, e_7_4 = 32,
+    e_3_8 = 64, e_5_8 = 128, e_6_8 = 256, e_7_8 = 512, e_9_8 = 1024, e_12_8 = 2048
+  };
+
+  Tmeter(Emeter meter = e_none) : m_meter(meter) {}
+
+  Emeter meter() const { return m_meter; }
+
+  int upper() const; /**< Returns upper digit of time signature  */
+  int lower() const; /**< Returns lower digit of time signature  */
+
+    /** Returns numeric value representing duration of single measure,
+     * which is based on Trhythm calculation (RVALUE)
+     * 3/4 is 72, 4/4 is 96 (RVALUE), etc. */
+  quint16 duration() const;
+
+  QPixmap pixmap(int fontSize, const QColor& c = -1); /**< Returns QPixmap of this time signature with given font size and color. */
+
+  void toXml(QXmlStreamWriter& xml) const;
+  bool fromXml(QXmlStreamReader& xml);
+
+  void debug(const QString& text = QString()); /**< Prints current meter to std out with given text */
+
+  bool operator==(const Tmeter& m) const { return meter() == m.meter(); }
+  bool operator!=(const Tmeter& m) const { return meter() != m.meter(); }
+
+private:
+  Emeter        m_meter;
+};
+
+#endif // TMETER_H
---
+++ src/libs/scorek/music/trhythm.h
@@ -0,0 +1,86 @@
+/***************************************************************************
+ *   Copyright (C) 2014 by Tomasz Bojczuk                                  *
+ *   seelook@gmail.com                                                     *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
+ ***************************************************************************/
+
+#ifndef TRHYTHM_H
+#define TRHYTHM_H
+
+#include <QString>
+#include <cmath>
+
+
+const std::string rhythmStrings [6] = {"", "whole", "half", "quarter", "eighth", "16th"};
+
+
+/** 
+ * This class describes musical note value (relative duration)
+ */
+class Trhythm
+{
+
+public:
+	
+	enum EnoteValue {
+    e_none = 0, e_whole = 1, e_half = 2, e_quarter = 4, e_eighth = 8, e_sixteenth = 16
+	}; /** Describes note duration */
+	
+			/** Creates note value, by default it is quarter (without dot) */
+	Trhythm(EnoteValue nVal = e_quarter, bool rest = false, bool dot = false) :
+		m_noteVal(nVal), m_isRest(rest), m_hasDot(dot)
+	{}
+	
+			/** Sets rhythm parameters */
+	void setRhythm(EnoteValue nVal, bool rest = false, bool dot = false) {
+		m_noteVal = nVal;
+		m_isRest = rest;
+		m_hasDot = dot;
+  }
+	
+	EnoteValue rhythm() { return m_noteVal; }
+	void setNoteValue(EnoteValue nVal) { m_noteVal = nVal; }
+	
+			/** It converts QString into rhythm value */
+	void setNoteValue(const std::string& nVal) {
+		for (int i = 0; i < 6; ++i) {
+			if (nVal == rhythmStrings[i]) {
+				m_noteVal = (EnoteValue)i;
+				return;
+			}
+		}
+	}
+	
+	bool hasDot() { return m_hasDot; }
+	void setDot(bool dot) { m_hasDot = dot; }
+	
+	bool isRest() { return m_isRest; }
+	void setRest(bool rest) { m_isRest = rest; }
+	
+	QString xmlType() {
+		for (int i = 1; i < 6; ++i) {
+			if (pow(2, i - 1) == (int)m_noteVal)
+				return QString::fromStdString(rhythmStrings[i]);
+		}
+		return QString();
+  }
+
+
+private:
+  EnoteValue 							m_noteVal;
+  bool 										m_hasDot, m_isRest;
+};
+
+#endif // TRHYTHM_H
