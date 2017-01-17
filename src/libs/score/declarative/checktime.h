40b08ac6a8b9765e680131def3ff6f7b2a569a42
SeeLook <SeeLook@localhost>
2017-01-17 10:19:51 +0100

    Staff line is rendered in separate class DeStaff. DeScore manages staves and keeps size of wrapping Flickable content up to date. Added missing library exports.

---
+++ src/libs/score/declarative/checktime.h
@@ -0,0 +1,21 @@
+#include <QtCore/qdebug.h>
+#include <QtCore/qelapsedtimer.h>
+
+/**
+ * @p CHECKTIME macro measures time spent on function/method
+ * Usage:
+ * CHECKTIME(
+ *  (...)
+ *  code to perform
+ * (...)
+ * )
+ */
+#define CONCAT_(x,y) x##y
+#define CONCAT(x,y) CONCAT_(x,y)
+#define CHECKTIME(x)  \
+    QElapsedTimer CONCAT(sb_, __LINE__); \
+    CONCAT(sb_, __LINE__).start(); \
+    x \
+    qDebug() << __FUNCTION__ << ":" << __LINE__ << " Elapsed time: " <<  CONCAT(sb_, __LINE__).nsecsElapsed() / 1000 << " μs.";
+
+
--- src/libs/score/declarative/destaff.h
+++ src/libs/score/declarative/destaff.h
@@ -16,71 +16,43 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
  ***************************************************************************/
 
-#ifndef DESCORE_H
-#define DESCORE_H
+#ifndef DESTAFF_H
+#define DESTAFF_H
 
 
 #include <nootkacoreglobal.h>
-
-#include "destaff.h"
-#include <QtQuick/qquickitem.h>
-#include <music/tclef.h>
-#include <music/tmeter.h>
+#include <QtQuick/qquickpainteditem.h>
 
 
+class DeScore;
 class TscoreScene;
 class TscoreStaff;
-class DeStaff;
 
 
-class NOOTKACORE_EXPORT DeScore : public QQuickItem
+/**
+ * @class DeStaff renders score scene with single staff
+ * into @p QPainter of Scene Graph.
+ * It has no representation in QML,
+ * it is managed by parent class @p DeScore
+ */
+class NOOTKACORE_EXPORT DeStaff : public QQuickPaintedItem
 {
+
   Q_OBJECT
 
-  Q_PROPERTY(Tclef::EclefType clef READ clef WRITE setClef NOTIFY clefChanged)
-  Q_PROPERTY(Tmeter::Emeter meter READ meter WRITE setMeter NOTIFY meterChanged)
-
-
-  friend class DeStaff;
-
-
 public:
-  explicit DeScore(QQuickItem* parent = nullptr);
-  ~DeScore();
+  explicit DeStaff(DeScore* score, int staffNr, QQuickItem* parent = nullptr);
+  ~DeStaff();
 
-  Tclef::EclefType clef();
-  void setClef(Tclef::EclefType c);
-
-  Tmeter::Emeter meter();
-  void setMeter(Tmeter::Emeter m);
-
-  qint8 keySignature();
-  void setKeySignature(qint8 k);
-
+  TscoreStaff* staff() { return m_staff; }
 
   void geometryChanged(const QRectF& newGeometry, const QRectF& oldGeometry) override;
-
-
-signals:
-  void clefChanged();
-  void meterChanged();
-  void keySignatureChanged();
-
-
-protected:
-  void itemChange(QQuickItem::ItemChange change, const QQuickItem::ItemChangeData& value) override;
-  void componentComplete() override;
-
-  void hoverMoveEvent(QHoverEvent* event) override;
-
-  void deleteKeySlot();
-
-  TscoreStaff* staff(int id) { return m_staves[id]->staff(); }
-
+  void paint(QPainter* painter) override;
 
 private:
-  QList<DeStaff*>          m_staves;
-  QQuickItem              *m_flickContent; /**<  Pointer to Flickable content item (plane of whole score) */
+  DeScore                 *m_score;
+  TscoreScene             *m_scene;
+  TscoreStaff             *m_staff;
 };
 
-#endif // DESCORE_H
+#endif // DESTAFF_H
