d38e7e2727e845a630da4dc67ad0d74a8712b87c
SeeLook <945374+SeeLook@users.noreply.github.com>
2017-04-04 22:07:19 +0200

    Use const QString reference, doc code layout. Revert initialization of global key name suffixes

--- src/libs/core/music/tkeysignature.h
+++ src/libs/core/music/tkeysignature.h
@@ -30,39 +30,45 @@ class QXmlStreamWriter;
 
 
 /**
-* This class describes a key signature.
-* It keeps it in char type. Also it keeps info about mode (minor/major)
-*/
+ * This class describes a key signature.
+ * It keeps it in char type. Also it keeps info about mode (minor/major)
+ */
 class NOOTKACORE_EXPORT TkeySignature
 {
 public:
     TkeySignature();
     TkeySignature(char keyS, bool isMinor = false);
 
-        /** Lists of keys names*/
+        /**
+         * Lists of keys names
+         */
     static QString majorNames[15], minorNames[15];
 
-        /** Array with definitions of each scale.
-        * @p scalesDefArr[7] is C-major/a-minor and has 7 times 0 (no accids).
-        * The order os from 7b [0] to 7# [15]*/
+        /**
+         * Array with definitions of each scale.
+         * @p scalesDefArr[7] is C-major/a-minor and has 7 times 0 (no accids).
+         * The order os from 7b [0] to 7# [15]
+         */
     static const char scalesDefArr[15][7];
 
-        /** Those smart arrays keep which note in @p scalesDefArr is 1-st in key scale.
-        * F.e. For G-major is @p majorKeys[8] = 4,
-        * like @p scalesDefArr[8][4] point on accid of a G note.
-        * This is faster way then calculate it and takes less memory.*/
+        /**
+         * Those smart arrays keep which note in @p scalesDefArr is 1-st in key scale.
+         * F.e. For G-major is @p majorKeys[8] = 4,
+         * like @p scalesDefArr[8][4] point on accid of a G note.
+         * This is faster way then calculate it and takes less memory.
+         */
     static const char majorKeys[15], minorKeys[15];
 
         /**
-        * This method fulfills majorNames and minorNames arrays
-        * appropriate keys names depends on naming style
-        * and suffixes.
-        * Convention of suffixes is:
-        * - empty string "" means - suffix id default for language and taken from translations
-        * - string with space " " means - suffix is empty
-        * - any other string means - it is as it is
-        */
-    static void setNameStyle(Tnote::EnameStyle style, QString majSuf = QString(), QString minSuf = QString());
+         * This method fulfills majorNames and minorNames arrays
+         * appropriate keys names depends on naming style
+         * and suffixes.
+         * Convention of suffixes is:
+         * - empty string "" means - suffix id default for language and taken from translations
+         * - string with space " " means - suffix is empty
+         * - any other string means - it is as it is
+         */
+    static void setNameStyle(Tnote::EnameStyle style, const QString& majSuf = QString(), const QString& minSuf = QString());
     static QString majorSufixTxt() { return QObject::tr("major"); }
     static QString minorSufixTxt() { return QObject::tr("minor"); }
 
@@ -71,7 +77,9 @@ public:
     QString getMajorName() { return majorNames[m_key+7]; }
     QString getMinorName() { return minorNames[m_key+7]; }
 
-      /** Returns minor or major name depend on m_isMinor state. */
+      /**
+       * Returns minor or major name depend on m_isMinor state.
+       */
     QString getName() { 
       if (m_isMinor)
           return getMinorName();
@@ -79,15 +87,19 @@ public:
           return getMajorName();
     }
 
-        /** Return QString with accidentals number and their symbol.
-        * f.e.: 7# for @p m_key = 7 (Cis-major).
-        * @p inHtml points when HTML syntax is used. If true # and b symbols
-        * are formatted to better view. */
+        /**
+         * Return QString with accidentals number and their symbol.
+         * f.e.: 7# for @p m_key = 7 (Cis-major).
+         * @p inHtml points when HTML syntax is used. If true # and b symbols
+         * are formatted to better view.
+         */
     QString accidNumber(bool inHtml = false);
 
-        /** Checks is given note @p n in given key signature @p k.
-        * In the method it checks all possibilities of the note n (flats, sharps, natural,
-        * and returns either founded note or Tnote(0,0,0) if didn't find.*/
+        /**
+         * Checks is given note @p n in given key signature @p k.
+         * In the method it checks all possibilities of the note n (flats, sharps, natural,
+         * and returns either founded note or Tnote(0,0,0) if didn't find.
+         */
     static Tnote inKey(TkeySignature k, Tnote n);
     Tnote inKey(Tnote n);
     bool isMinor() { return m_isMinor; }
@@ -95,16 +107,19 @@ public:
 
     char value() { return m_key; }
 
-        /** Returns base (tonic) note of this key signature, i.e. for a-minor key it is note 'a'.
-        * By default note is in small octave, but it can be set through @p octave parameter. */
+        /**
+         * Returns base (tonic) note of this key signature, i.e. for a-minor key it is note 'a'.
+         * By default note is in small octave, but it can be set through @p octave parameter.
+         */
     Tnote tonicNote(int octave = 0);
 
-    /** Adds 'key' key to XML stream compatible with MusicXML format with current key signature
-      * <key>
-      * <fifths>-2</fifths>
-      * <mode>major</mode>
-      * </key>  
-      */
+    /**
+     * Adds 'key' key to XML stream compatible with MusicXML format with current key signature
+     * <key>
+     * <fifths>-2</fifths>
+     * <mode>major</mode>
+     * </key>  
+     */
   void toXml(QXmlStreamWriter& xml);
   void fromXml(QXmlStreamReader& xml); /** Reads this key signature from XML stream  */
 
