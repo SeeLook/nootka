d6ce08e1a9907dc9e4dcce8bb21472ced19e582c
SeeLook <945374+SeeLook@users.noreply.github.com>
2017-11-24 21:42:47 +0100

    Storing question/answer types as a single byte value of bits combinations instead of Boolean array. Added helper methods

--- src/libs/core/exam/tqatype.cpp
+++ src/libs/core/exam/tqatype.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2011-2014 by Tomasz Bojczuk                             *
+ *   Copyright (C) 2011-2017 by Tomasz Bojczuk                             *
  *   seelook@gmail.com                                                     *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
@@ -18,68 +18,70 @@
 
 
 #include "tqatype.h"
-#include <QXmlStreamWriter>
-#include <QVariant>
+#include <QtCore/qxmlstream.h>
+#include <QtCore/qvariant.h>
+#include <QtCore/qmath.h>
 
-TQAtype::TQAtype()
-{
-    TQAtype(false, false, false, false);
-}
 
 TQAtype::TQAtype(bool _asNote, bool _asName, bool _asFretPos, bool _asSound)
 {
-    m_typeArr[0] = _asNote;
-    m_typeArr[1] = _asName;
-    m_typeArr[2] = _asFretPos;
-    m_typeArr[3] = _asSound;
-    m_index = 0;
+  m_value = (_asNote ? 1 : 0) + (_asName ? 2 : 0) + (_asFretPos ? 4 : 0) + (_asSound ? 8 : 0);
 }
 
+
+TQAtype::TQAtype(int val) {
+  m_value = static_cast<quint16>(val);
+}
+
+
 TQAtype::Etype TQAtype::next() {
-    do {
-        m_index++;
-        if (m_index == 4) m_index = 0;
-    } while ( !m_typeArr[m_index] );
-    return (Etype)m_index;
+  do {
+      m_index++;
+      if (m_index == 4)
+        m_index = 0;
+  } while (!(m_value & static_cast<quint8>(qPow(2.0, static_cast<qreal>(m_index)))));
+
+  return static_cast<Etype>(m_index);
 }
 
 
 TQAtype::Etype TQAtype::randNext() {
-    m_index = (qrand() % 4) - 1;
-    return next();
+  m_index = (qrand() % 4) - 1;
+  return next();
 }
 
 
 void TQAtype::toXml(int id, QXmlStreamWriter& xml) {
-  xml.writeStartElement("qaType");
-    xml.writeAttribute("id", QVariant(id).toString());
-    xml.writeAttribute("score", QVariant(isNote()).toString());
-    xml.writeAttribute("name", QVariant(isName()).toString());
-    xml.writeAttribute("guitar", QVariant(isFret()).toString());
-    xml.writeAttribute("sound", QVariant(isSound()).toString());
+  xml.writeStartElement(QStringLiteral("qaType"));
+    xml.writeAttribute(QStringLiteral("id"), QVariant(id).toString());
+    xml.writeAttribute(QStringLiteral("score"), QVariant(isNote()).toString());
+    xml.writeAttribute(QStringLiteral("name"), QVariant(isName()).toString());
+    xml.writeAttribute(QStringLiteral("guitar"), QVariant(isFret()).toString());
+    xml.writeAttribute(QStringLiteral("sound"), QVariant(isSound()).toString());
   xml.writeEndElement(); // qaType
 }
 
 
 int TQAtype::fromXml(QXmlStreamReader& xml) {
-  int id = QVariant(xml.attributes().value("id").toString()).toInt();
-  setAsNote(QVariant(xml.attributes().value("score").toString()).toBool());
-  setAsName(QVariant(xml.attributes().value("name").toString()).toBool());
-  setAsFret(QVariant(xml.attributes().value("guitar").toString()).toBool());
-  setAsSound(QVariant(xml.attributes().value("sound").toString()).toBool());
+  int id = QVariant(xml.attributes().value(QStringLiteral("id")).toString()).toInt();
+  setAsNote(QVariant(xml.attributes().value(QStringLiteral("score")).toString()).toBool());
+  setAsName(QVariant(xml.attributes().value(QStringLiteral("name")).toString()).toBool());
+  setAsFret(QVariant(xml.attributes().value(QStringLiteral("guitar")).toString()).toBool());
+  setAsSound(QVariant(xml.attributes().value(QStringLiteral("sound")).toString()).toBool());
   xml.skipCurrentElement();
   return id;
 }
 
 
 QDataStream &operator << (QDataStream &out,TQAtype &qatype) {
-    out << qatype.isNote() << qatype.isName() << qatype.isFret() << qatype.isSound();
-    return out;
+  out << qatype.isNote() << qatype.isName() << qatype.isFret() << qatype.isSound();
+  return out;
 }
 
+
 QDataStream &operator >> (QDataStream &in, TQAtype &qatype) {
-    bool b[4];
-    in >> b[0] >> b[1] >> b[2] >> b[3];
-    qatype = TQAtype(b[0], b[1], b[2], b[3]);
-    return in;
+  bool b[4];
+  in >> b[0] >> b[1] >> b[2] >> b[3];
+  qatype = TQAtype(b[0], b[1], b[2], b[3]);
+  return in;
 }
