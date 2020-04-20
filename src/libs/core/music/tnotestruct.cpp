42097dd14447a7827ce9d8b87675fa749be6f1bf
SeeLook <seelook@gmail.com>
2020-04-20 09:07:15 +0200

    Clean code, modern casting

--- src/libs/core/music/tnotestruct.cpp
+++ src/libs/core/music/tnotestruct.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2014-2017 by Tomasz Bojczuk                             *
+ *   Copyright (C) 2014-2020 by Tomasz Bojczuk                             *
  *   seelook@gmail.com                                                     *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
@@ -37,15 +37,15 @@ void TnoteStruct::init(int _index, int chunkNr, qreal floatPitch) {
 
 void TnoteStruct::update(int chunkNr, qreal floatPitch, float vol) {
   if (numChunks() == 2) {
-    pitchF = floatPitch; // is better to take base pitch not from first chunks
-    basePitch = qRound(pitchF);
+    pitchF = floatPitch;
+    basePitch = qRound(pitchF); // it is better to take base pitch not from first chunks
   }
   m_pList << floatPitch;
   endChunk = chunkNr;
   maxVol = qMax<float>(maxVol, vol);
   if (numChunks() > 3) // skip first 3 chunks - Tartini may detected a note with low volume
     minVol = qMin<float>(minVol, vol);
-  if (qAbs<qreal>(floatPitch - (qreal)basePitch) < qAbs<qreal>(bestPitch - (qreal)basePitch))
+  if (qAbs(floatPitch - static_cast<qreal>(basePitch)) < qAbs(bestPitch - static_cast<qreal>(basePitch)))
     bestPitch = floatPitch;
 }
 
