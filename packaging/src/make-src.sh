0e9d3639aeb9b54339bf0be0e481c3095d46c76a
SeeLook <seelook@gmail.com>
2020-10-21 12:39:23 +0200

    fixup! [packaging] remove obtaining mercurial number

--- packaging/src/make-src.sh
+++ packaging/src/make-src.sh
@@ -26,7 +26,7 @@ cp -r $SRC_DIR/mime $DST_DIR
 cp -r $SRC_DIR/packaging $DST_DIR
 cp -r $SRC_DIR/picts $DST_DIR
 mkdir $DST_DIR/sounds
-mkdir -p $DST_DIR/.hg/cache
+
 cp $SRC_DIR/sounds/*.ogg $DST_DIR/sounds/
 cp $SRC_DIR/sounds/*.raw48-16 $DST_DIR/sounds/
 cp -r $SRC_DIR/src $DST_DIR
