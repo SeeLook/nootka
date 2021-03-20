c4df0fca25d192ed61ea9e781f40eca10327a228
SeeLook <seelook@gmail.com>
2021-03-20 22:53:20 +0100

    Copy all dll-s to insatller - attempt to find missing one

--- packaging/mingw-w64/nootka-mingw-w64.sh
+++ packaging/mingw-w64/nootka-mingw-w64.sh
@@ -2,21 +2,25 @@
 
 echo "-- copying GCC libraries"
 
-cp /usr/x86_64-w64-mingw32/bin/libstdc++-6.dll installs/
-cp /usr/x86_64-w64-mingw32/bin/libwinpthread-1.dll installs/
-cp /usr/x86_64-w64-mingw32/bin/libgcc_s_seh-1.dll installs/
-cp /usr/x86_64-w64-mingw32/bin/libiconv-2.dll installs/
-cp /usr/x86_64-w64-mingw32/bin/libharfbuzz*.dll installs/
-cp /usr/x86_64-w64-mingw32/bin/libpng*.dll installs/
-cp /usr/x86_64-w64-mingw32/bin/zlib1.dll installs/
-cp /usr/x86_64-w64-mingw32/bin/libpcre*.dll installs/
-cp /usr/x86_64-w64-mingw32/bin/libgraphite2.dll installs/
-cp /usr/x86_64-w64-mingw32/bin/libbz2-1.dll installs/
-cp /usr/x86_64-w64-mingw32/bin/libfreetype-6.dll installs/
-cp /usr/x86_64-w64-mingw32/bin/libglib-2.0-0.dll installs/
-cp /usr/x86_64-w64-mingw32/bin/libintl-8.dll installs/
-cp /usr/x86_64-w64-mingw32/bin/libssp-0.dll installs/
-cp /usr/x86_64-w64-mingw32/bin/libjpeg-8.dll installs/
+# libgobject-2.0-0.dll
+# cp /usr/x86_64-w64-mingw32/bin/libstdc++-6.dll installs/
+# cp /usr/x86_64-w64-mingw32/bin/libwinpthread-1.dll installs/
+# cp /usr/x86_64-w64-mingw32/bin/libgcc_s_seh-1.dll installs/
+# cp /usr/x86_64-w64-mingw32/bin/libiconv-2.dll installs/
+# cp /usr/x86_64-w64-mingw32/bin/libharfbuzz*.dll installs/
+# cp /usr/x86_64-w64-mingw32/bin/libpng*.dll installs/
+# cp /usr/x86_64-w64-mingw32/bin/zlib1.dll installs/
+# cp /usr/x86_64-w64-mingw32/bin/libpcre*.dll installs/
+# cp /usr/x86_64-w64-mingw32/bin/libgraphite2.dll installs/
+# cp /usr/x86_64-w64-mingw32/bin/libbz2-1.dll installs/
+# cp /usr/x86_64-w64-mingw32/bin/libfreetype-6.dll installs/
+# cp /usr/x86_64-w64-mingw32/bin/libglib-2.0-0.dll installs/
+# cp /usr/x86_64-w64-mingw32/bin/libintl-8.dll installs/
+# cp /usr/x86_64-w64-mingw32/bin/libssp-0.dll installs/
+# cp /usr/x86_64-w64-mingw32/bin/libjpeg-8.dll installs/
+
+# Copy all libraries for testing
+cp /usr/x86_64-w64-mingw32/bin/lib*.dll installs/
 
 echo "-- copying Qt5 libraries"
 QT_LIBS=(Qt5Core  Qt5Gui  Qt5Network  Qt5PrintSupport  Qt5Qml  Qt5Quick  Qt5QuickControls2  Qt5QuickTemplates2  Qt5Widgets Qt5QmlModels Qt5Svg Qt5QmlWorkerScript)
@@ -42,10 +46,10 @@ do
   cp -r /usr/x86_64-w64-mingw32/lib/qt/qml/$mod installs/
 done
 
-echo "-- remove unused parts of QML modules"
-rm -r installs/Qt/labs/location
-rm -r installs/QtQuick/Particles.2
-rm -r installs/QtQuick/Shapes
-rm installs/platforms/qdirect2d.dll
-rm installs/platforms/qminimal.dll
-rm installs/platforms/qoffscreen.dll
+# echo "-- remove unused parts of QML modules"
+# rm -r installs/Qt/labs/location
+# rm -r installs/QtQuick/Particles.2
+# rm -r installs/QtQuick/Shapes
+# rm installs/platforms/qdirect2d.dll
+# rm installs/platforms/qminimal.dll
+# rm installs/platforms/qoffscreen.dll
