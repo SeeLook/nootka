fd4b792833c75f3a1ebba203220b42408a6050fd
SeeLook <seelook@gmail.com>
2021-07-26 21:54:29 +0200

    Improve script for updating translations

--- lang/lang-update.sh
+++ lang/lang-update.sh
@@ -7,11 +7,13 @@ SRC_DIR="../src"
 
 
 if [ ! -f "$SRC_DIR/main.cpp" ]; then
-  echo "Run script into lang dir !!!!"
+  echo "Run script from the inside 'lang' directory !!!!"
   exit
 fi
 
 for TS in $(ls nootka_*.ts); do
   echo $TS
-#   lupdate -recursive $SRC_DIR -ts $TS
+  lupdate -recursive $SRC_DIR -ts $TS
 done
+
+lupdate -no-obsolete -recursive $SRC_DIR -ts template-nootka_xx.ts
