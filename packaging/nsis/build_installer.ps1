7587feec756516d3dc43e2283d239c648ccad717
SeeLook <seelook@gmail.com>
2021-08-19 10:34:15 +0200

    Shush wget

--- packaging/nsis/build_installer.ps1
+++ packaging/nsis/build_installer.ps1
@@ -1,12 +1,12 @@
 # Powershell
 
 echo "--- Getting ZLIB"
-wget "https://www.opencode.net/seelook/nootka-build/-/raw/master/3rdParty/zlib.zip"
+wget -q "https://www.opencode.net/seelook/nootka-build/-/raw/master/3rdParty/zlib.zip"
 Expand-Archive -Path zlib.zip -DestinationPath zlib
 Move-Item -Path .\zlib\zlib\*.* -Destination .\zlib\
 
 echo "--- Getting 3rd party libraries (FFTW, OGG, VORBIS)"
-wget "https://www.opencode.net/seelook/nootka-build/-/raw/master/3rdParty/win32-fftw-ogg-vorbis.tar.gz"
+wget -q "https://www.opencode.net/seelook/nootka-build/-/raw/master/3rdParty/win32-fftw-ogg-vorbis.tar.gz"
 tar -xzf .\win32-fftw-ogg-vorbis.tar.gz
 
 mkdir build
